d41fe86065860ae7393146dc3b8ee0dd4b786266
khj <hongjung.kim.dev@gmail.com>
2022-04-18 03:08:20 +0900

    build: 개발용 실시간 리로드 서버, 소스맵, 환경변수 관련 webpack 설정

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,38 @@
-const path = require("path");
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
+const dotenv = require('dotenv');
+const webpack = require('webpack');
+dotenv.config();
+
+const path = require('path');
 module.exports = {
-  mode: "development",
-  entry: "./src/app.js",
+  mode: 'development',
+  entry: './src/app.js',
   output: {
-    path: path.resolve(__dirname, "dist"),
-    filename: "bundle.js",
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js',
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './index.html',
+    }),
+    new CleanWebpackPlugin(),
+    new webpack.DefinePlugin({
+      'process.env': JSON.stringify(process.env),
+    }),
+  ],
+  devtool: 'inline-source-map',
+  devServer: {
+    static: {
+      directory: path.join(__dirname, 'dist'),
+    },
+    client: {
+      overlay: true,
+    },
+    port: 8080,
+    historyApiFallback: true,
+    open: true,
   },
   module: {
     rules: [
@@ -13,7 +40,14 @@ module.exports = {
         test: /\.js$/,
         exclude: /node_modules/,
         use: {
-          loader: "babel-loader",
+          loader: 'babel-loader',
+        },
+      },
+      {
+        test: /\.(png|jpg|gif)$/i,
+        loader: 'file-loader',
+        options: {
+          name: '[name].[ext]?[hash]',
         },
       },
     ],
