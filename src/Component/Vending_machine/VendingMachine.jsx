bd2df8cb9784486801011ef2553466b8339e9ac5
godhyeongman <aaaa4000@naver.com>
2022-05-10 00:31:50 +0900

    refactor: 자판기 인터페이스 컴포넌트 분리

--- src/Component/Vending_machine/VendingMachine.jsx
+++ src/Component/Vending_machine/VendingMachine.jsx
@@ -1,20 +1,22 @@
-import React, {useState} from 'react';
+import React from 'react';
 import styled from 'styled-components';
 
 import {Products} from './Products';
-import {test} from '../../mocks/ProductData';
+import {VendingMachineInterface} from './VendingMachineInterface';
+
+import {PRODUCTS_DATA} from '../../mocks/ProductData';
+
+const createProducts = ProductsList => {
+  return ProductsList.map(productData => (
+    <Products title={productData.title} price={productData.price} />
+  ));
+};
 
 export const VendingMachine = () => {
   return (
     <VendingMachineWrapper>
-      <ProductList>
-        {test.map(productData => (
-          <Products title={productData.title} price={productData.price} />
-        ))}
-      </ProductList>
-      <VM_InterFace>
-        <VM_MoneyInput />
-      </VM_InterFace>
+      <ProductList>{createProducts(PRODUCTS_DATA)}</ProductList>
+      <VendingMachineInterface />
     </VendingMachineWrapper>
   );
 };
@@ -22,33 +24,15 @@ export const VendingMachine = () => {
 const VendingMachineWrapper = styled.div`
   width: 1000px;
   height: 1000px;
-  border: 3px solid black;
   display: flex;
+  border: 5px solid black;
 `;
 
 const ProductList = styled.ul`
   padding: 10px;
   width: 60%;
-  border-right: 3px solid black;
+  border-right: 5px solid black;
   display: flex;
   justify-content: space-around;
   flex-wrap: wrap;
-`;
-
-const VM_InterFace = styled.ul`
-  width: 40%;
-  display: flex;
-  align-items: center;
-  flex-direction: column;
-  padding 10px
-`;
-
-const VM_MoneyInput = styled.input`
-  width: 200px;
-  height: 80px;
-  font-size: 20px;
-  border-radius: 20px;
-  padding 10px ;
-
-
 `;
---
+++ src/Component/Vending_machine/VendingMachineInterface.jsx
@@ -0,0 +1,55 @@
+import React from 'react';
+import styled from 'styled-components';
+
+export const VendingMachineInterface = () => {
+  return (
+    <VM_Wrapper>
+      <VM_MoneyInput />
+      <VM_RefundBtn>잔액 반환</VM_RefundBtn>
+      <VM_History></VM_History>
+    </VM_Wrapper>
+  );
+};
+
+const VM_Wrapper = styled.ul`
+  width: 40%;
+  display: flex;
+    align-items: center;
+  justify-content: space-around;
+  flex-direction: column;
+    padding 10px
+`;
+
+const VM_MoneyInput = styled.input`
+  width: 300px;
+  height: 80px;
+  font-size: 20px;
+  border-radius: 20px;
+  padding: 10px;
+`;
+
+const VM_RefundBtn = styled.button`
+  width: 300px;
+  height: 80px;
+  font-size: 20px;
+  border-radius: 20px;
+  background-color: white;
+  padding: 10px;
+
+  cursor: pointer;
+  :hover {
+    background-color: rgba(0, 0, 0, 0.4);
+  }
+
+  :active {
+    background-color: rgba(0, 0, 0, 0.2);
+  }
+`;
+
+const VM_History = styled.div`
+  width: 300px;
+  height: 500px;
+  border: 1px solid black;
+  border-radius: 20px;
+  padding: 10px;
+`;
