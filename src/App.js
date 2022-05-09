b0a9a50ea5a6368e02633111173499cae9728b34
jm <jm.dev.study@gmail.com>
2022-05-10 02:47:43 +0900

    Feat: 페이지 이동을 위한 Navigation 컴포넌트 구현 - useLocation을 사용해서 현재 페이지와 id가 일치하는 태그에 배경색을 주도록 구현
    Related to: #3

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { ThemeProvider } from 'styled-components';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import GlobalStyle from 'theme/GlobalStyles';
 import theme from 'theme/theme';
+import Navigation from 'components/Navigation';
 import VendingMachine from 'Pages/VendingMachine';
 import Wallet from 'Pages/Wallet';
 import NotFound from 'Pages/NotFound';
@@ -11,6 +12,7 @@ function App() {
     <ThemeProvider theme={theme}>
       <GlobalStyle />
       <BrowserRouter>
+        <Navigation />
         <Routes>
           <Route path="/" element={<VendingMachine />} />
           <Route path="wallet" element={<Wallet />} />
