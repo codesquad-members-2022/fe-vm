981b4ec85e657dd82b9c519f969553ee7313d4a9
godhyeongman <aaaa4000@naver.com>
2022-05-09 17:38:46 +0900

    feat: 자판기, 지갑, 토글버튼 컴포넌트 마크업

--- src/App.js
+++ src/App.js
@@ -2,10 +2,11 @@ import React from 'react';
 import styled from 'styled-components';
 import {VendingMachine} from './Component/Vending_machine';
 import {Wallet} from './Component/Wallet';
+import {UiChangeBtn} from './Component/Toggle';
 
 const AppWrapper = styled.div`
   display: flex;
-  justify-contents: center;
+  justify-content: center;
   align-items: center;
   flex-direction: column;
 `;
@@ -13,6 +14,7 @@ const AppWrapper = styled.div`
 const App = () => {
   return (
     <AppWrapper>
+      <UiChangeBtn />
       <VendingMachine />
       {/* <Wallet /> */}
     </AppWrapper>
