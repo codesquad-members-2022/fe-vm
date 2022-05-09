a31327f11c20a90a51e631a66d16c637ff2981f5
healtheloper <58503584+healtheloper@users.noreply.github.com>
2022-05-09 17:34:41 +0900

    data: mocks data add

---
+++ src/mocks/vmItems.js
@@ -0,0 +1,104 @@
+const vmItems = [
+  {
+    name: '콜라',
+    price: 500,
+    amount: 10,
+  },
+  {
+    name: '사이다',
+    price: 1000,
+    amount: 10,
+  },
+  {
+    name: '환타 파인',
+    price: 400,
+    amount: 10,
+  },
+  {
+    name: '환타 그레이프',
+    price: 300,
+    amount: 10,
+  },
+  {
+    name: '레몬에이드',
+    price: 900,
+    amount: 10,
+  },
+  {
+    name: '봉봉',
+    price: 1200,
+    amount: 10,
+  },
+  {
+    name: '코코아주스',
+    price: 1000,
+    amount: 10,
+  },
+  {
+    name: '콜라 제로',
+    price: 1000,
+    amount: 10,
+  },
+  {
+    name: '파워에이드',
+    price: 2000,
+    amount: 10,
+  },
+  {
+    name: '초코에몽',
+    price: 1000,
+    amount: 10,
+  },
+  {
+    name: '가나초코우유',
+    price: 7000,
+    amount: 10,
+  },
+  {
+    name: '딸기우유',
+    price: 600,
+    amount: 10,
+  },
+  {
+    name: '딸바주스',
+    price: 1000,
+    amount: 10,
+  },
+  {
+    name: '바나나우유',
+    price: 500,
+    amount: 10,
+  },
+  {
+    name: '커피우유',
+    price: 1000,
+    amount: 10,
+  },
+  {
+    name: '알로에',
+    price: 1200,
+    amount: 10,
+  },
+  {
+    name: '나랑드사이다',
+    price: 1000,
+    amount: 10,
+  },
+  {
+    name: '웰치스 포도',
+    price: 1000,
+    amount: 10,
+  },
+  {
+    name: '쿨피스',
+    price: 400,
+    amount: 10,
+  },
+  {
+    name: '무야호',
+    price: 1000,
+    amount: 10,
+  },
+];
+
+export default vmItems;
