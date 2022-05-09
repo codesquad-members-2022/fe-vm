499adbed3fa3feac919950e8e6d5439ed43884cd
healtheloper <58503584+healtheloper@users.noreply.github.com>
2022-05-09 17:29:13 +0900

    style: header style

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,24 +1,74 @@
 import { Link, Outlet } from 'react-router-dom';
+import styled from 'styled-components';
+
+import COLORS from 'constants/colors';
 
 const Layout = () => (
   <>
-    <header>
-      <h1>코쿼 자판기</h1>
+    <Header>
+      <HeaderTitle>코쿼 자판기</HeaderTitle>
       <nav>
-        <ul>
-          <li>
-            <Link to='/vendingMachine'>자판기</Link>
-          </li>
-          <li>
-            <Link to='/wallet'>지갑</Link>
-          </li>
-        </ul>
+        <NavItems>
+          <NavItem>
+            <Link to='/vendingMachine'>
+              <Button>자판기</Button>
+            </Link>
+          </NavItem>
+          <NavItem>
+            <Link to='/wallet'>
+              <Button>지갑</Button>
+            </Link>
+          </NavItem>
+        </NavItems>
       </nav>
-    </header>
-    <div className='content'>
+    </Header>
+    <Content>
       <Outlet />
-    </div>
+    </Content>
   </>
 );
+
+const Header = styled.header`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  width: 100vw;
+  height: 10rem;
+  padding: 10px;
+  background-color: ${COLORS.WHITE};
+`;
+
+const HeaderTitle = styled.h1`
+  font-weight: 700;
+  font-size: 40px;
+  margin-bottom: 5px;
+`;
+
+const NavItems = styled.ul`
+  display: flex;
+  justify-content: center;
+  width: 20rem;
+`;
+
+const NavItem = styled.li`
+  margin: 0px 5px;
+  font-size: 20px;
+  width: 50%;
+`;
+
+const Button = styled.button`
+  font-size: 1em;
+  cursor: pointer;
+  width: 100%;
+  background-color: ${COLORS.SKY_BLUE};
+  border-radius: 5px;
+  color: ${COLORS.WHITE};
+  transition: background-color 0.2s;
+  &:hover {
+    background-color: ${COLORS.BLUE};
+  }
+`;
+const Content = styled.div``;
 
 export default Layout;
---
+++ src/constants/colors.js
@@ -0,0 +1,8 @@
+const COLORS = {
+  MAIN_BG: '#dfe6e9',
+  WHITE: '#fff',
+  SKY_BLUE: '#74b9ff',
+  BLUE: '#0984e3',
+};
+
+export default COLORS;
