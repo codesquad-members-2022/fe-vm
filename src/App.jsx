0c5c2041d1ea11e5f9a44fda473a32781f3fc3ea
godhyeongman <aaaa4000@naver.com>
2022-05-09 12:24:49 +0900

    design: reset 스타일 적용

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,22 @@
-import React, { useState, useEffect, useReducer } from 'react';
+import React from 'react';
+import styled from 'styled-components';
+import {VendingMachine} from './Component/Vending_machine';
+import {Wallet} from './Component/Wallet';
 
-function App() {
+const AppWrapper = styled.div`
+  display: flex;
+  justify-contents: center;
+  align-items: center;
+  flex-direction: column;
+`;
+
+const App = () => {
   return (
-    <>
-      <div>hello 도트!!</div>
-    </>
+    <AppWrapper>
+      <VendingMachine />
+      {/* <Wallet /> */}
+    </AppWrapper>
   );
-}
+};
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -2,10 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 
-if (process.env.NODE_ENV === 'development') {
-  const { worker } = require('./mocks/browser');
-  worker.start();
-}
+import './index.css';
+
+// if (process.env.NODE_ENV === 'development') {
+//   const { worker } = require('./mocks/browser');
+//   worker.start();
+// }
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
