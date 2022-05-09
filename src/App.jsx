e3c1259942bd9769740a4d688b028e58dcb80ea7
BB-choi <choibobae0809@gmail.com>
2022-05-09 12:27:15 +0900

    env: react-router-dom을 이용하여 Routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,23 @@
-import React from "react";
+import { Routes, Route } from "react-router-dom";
+import Test from "./Test";
 
-const Message = "첫번째 페이지입니다. App.jsx";
+const Message = () => {
+  return (
+    <div>
+      <div>첫번째 페이지입니다. App.jsx</div>
+    </div>
+  );
+};
 
 const App = () => {
-  return <div className="App">{Message}</div>;
+  return (
+    <div className="App">
+      <Routes>
+        <Route path="/" exact={true} element={<Message />} />
+        <Route path="/test" element={<Test />} />
+      </Routes>
+    </div>
+  );
 };
 
 export default App;
---
+++ src/Test.jsx
@@ -0,0 +1,5 @@
+const Test = () => {
+  return <div>여기는 Test.jsx입니다.</div>;
+};
+
+export default Test;
