25f4d36a534aa46d9870063b7c72aec01da770aa
BB-choi <choibobae0809@gmail.com>
2022-05-09 11:55:49 +0900

    fix: index.html 파비콘 관련 코드 삭제
    - manifest.json 삭제
    - favicon.ico 삭제

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,9 @@
 import React from "react";
 
+const Message = "첫번째 페이지입니다. App.jsx";
+
 const App = () => {
-  return <div className="App" />;
+  return <div className="App">{Message}</div>;
 };
 
 export default App;
