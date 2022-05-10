0d77e989b0e6190d513f860feb58fbe777751c90
ju-kkim <wndus8198@naver.com>
2022-05-11 02:57:43 +0900

    feat: BrowserRouter 추가

--- src/index.js
+++ src/index.js
@@ -2,11 +2,14 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { GlobalStyles } from './global.style'
 import App from './App'
+import { BrowserRouter } from 'react-router-dom'
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
   <React.StrictMode>
     <GlobalStyles />
-    <App />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </React.StrictMode>,
 )
