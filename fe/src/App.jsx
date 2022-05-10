a78432d17b0152d1eb4fcc427e24fd493c5b1705
yunho <kimyouknow@naver.com>
2022-05-10 15:50:14 +0900

    Feat: mock 데이터 추가 (mock-data -> first-pr)
    commit 5066297ece066179c550ae7529e4d39d63da2394
    Author: yunho <kimyouknow@naver.com>
    Date:   Mon May 9 23:43:50 2022 +0900
    
        Chore: mock데이터 변수명 변경
    
        pruduct_name -> product_name
    
    commit 32da444d15e2e9336f6496e8450f405e7617f28e
    Author: yunho <kimyouknow@naver.com>
    Date:   Mon May 9 23:43:21 2022 +0900
    
        Chore: prop-types 라이브러리 추가
    
    commit c5506d665f4a090cebd2e9b539554b78153baca5
    Author: yunho <kimyouknow@naver.com>
    Date:   Mon May 9 23:32:15 2022 +0900
    
        Chore: drinks, snacks mock데이터 추가

--- fe/src/App.jsx
+++ fe/src/App.jsx
@@ -1,12 +1,20 @@
 import React from 'react';
-import Button from '@mui/material/Button';
+import drinks from 'mock/drinks';
+import snacks from 'mock/snacks';
 
 function App() {
   return (
-    <div>
-      <Button variant="contained" color="primary">
-        Hello World
-      </Button>
+    <div style={{ display: 'flex' }}>
+      <ul>
+        {drinks.map(drink => (
+          <li key={drink.id}>{drink.pruduct_name}</li>
+        ))}
+      </ul>
+      <ul>
+        {snacks.map(snack => (
+          <li key={snack.id}>{snack.pruduct_name}</li>
+        ))}
+      </ul>
     </div>
   );
 }
