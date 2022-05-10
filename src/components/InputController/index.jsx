07f794f05de7aec1bc18b2ceb484a965a264bbd3
Cola <bcad1591@gmail.com>
2022-05-10 15:29:33 +0900

    Feat: Home 자식 컴포넌트 추가
    Dispaly: 제품 진열대
    InputController: 투입금과 반환버튼
    ActionLogs: 로깅 화면

---
+++ src/components/InputController/index.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const InputController = () => {
+  return <div>InputController</div>;
+};
+
+export default InputController;
--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -1,7 +1,25 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import styled from 'styled-components';
+
+import ActionLogs from '@/components/ActionLogs';
+import Display from '@/components/Display';
+import InputController from '@/components/InputController';
+import { VMContext } from '@/Provider/VMProvider';
+
+const HomeLayer = styled.div`
+  width: 1080px;
+`;
 
 const Home = () => {
-  return <div>Home</div>;
+  const value = useContext(VMContext);
+  console.log(value);
+  return (
+    <HomeLayer>
+      <Display />
+      <InputController />
+      <ActionLogs />
+    </HomeLayer>
+  );
 };
 
 export default Home;
