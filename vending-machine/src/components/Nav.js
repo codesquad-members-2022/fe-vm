5c6b93c1488b1b6930e468c793bf092b6022fd35
SeungHyun <fm10033@gmail.com>
2022-05-10 16:12:12 +0900

    Design : Nav 스타일링 적용
    버튼 호버시 색상 밝아지게  구현
    버튼  클릭시 색상 어두워지게 구현

--- vending-machine/src/components/Nav.js
+++ vending-machine/src/components/Nav.js
@@ -13,14 +13,23 @@ const NavBtns = styled.div`
   display: flex;
   align-items: center;
   justify-content: center;
+  gap: 20px;
+  height: 100px;
 `;
 const VendingMachineBtn = styled.button`
-  width: 100px;
+  width: 150px;
   height: 50px;
   border: none;
-  background: none;
+  border-radius: 5px;
+  background: ${({ theme }) => theme.colors.gray};
   font-weight: bold;
   font-size: 20px;
+  &:hover {
+    background: ${({ theme }) => theme.colors.lightGray};
+  }
+  &:active {
+    background: ${({ theme }) => theme.colors.darkGray};
+  }
 `;
 const WalletBtn = styled(VendingMachineBtn)``;
 export default Nav;
