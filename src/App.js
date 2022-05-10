481b0597db7d932fdff64120551007075bd90b60
decay95 <decay95@naver.com>
2022-05-11 00:17:01 +0900

    [feat] header 메뉴 클릭시 상태 변경 기능 구현

--- src/App.js
+++ src/App.js
@@ -1,7 +1,18 @@
+import { useState } from 'react';
+import { Header } from './components/header/header';
 import './App.css';
 
 function App() {
-  return;
-}
+  const [isVMClicked, setVMClicked] = useState(true);
+  const [isWalletClicked, setWalletClicked] = useState(true);
 
+  return (
+    <>
+      <Header
+        VMStateObj={{ isVMClicked, setVMClicked }}
+        WalletStateObj={{ isWalletClicked, setWalletClicked }}
+      />
+    </>
+  );
+}
 export default App;
---
+++ src/components/header/header.js
@@ -0,0 +1,35 @@
+import styled, { css } from 'styled-components';
+
+export const Header = ({ VMStateObj, WalletStateObj }) => {
+  const { isVMClicked, setVMClicked } = VMStateObj;
+  const { isWalletClicked, setWalletClicked } = WalletStateObj;
+
+  const handleVMClick = () => {
+    setVMClicked(!isVMClicked);
+  };
+
+  const handleWalletClick = () => {
+    setWalletClicked(!isWalletClicked);
+  };
+
+  return (
+    <div>
+      <VMButton isClick={isVMClicked} onClick={handleVMClick}>
+        자판기
+      </VMButton>
+      <WalletButton isClick={isWalletClicked} onClick={handleWalletClick}>
+        지갑
+      </WalletButton>
+    </div>
+  );
+};
+
+const VMButton = styled.button`
+  color: ${({ isClick }) => isClick && 'tomato'};
+  cursor: pointe;
+`;
+
+const WalletButton = styled.button`
+  color: ${({ isClick }) => isClick && 'tomato'};
+  cursor: pointe;
+`;
