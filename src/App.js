fe552fe4271f22ff6459002c93062f938220ae9e
otterp012 <otterp012@gmail.com>
2022-05-10 17:24:54 +0900

    [style] : App 컴포넌트 스타일 변경, container 활용

--- src/App.js
+++ src/App.js
@@ -1,5 +1,36 @@
+import Header from './components/layout/Header/Header';
+import Nav from './components/layout/Nav/Nav';
+import theme from './style/theme.js';
+import GlobalStyle from './style/GlobalStyle';
+import styled, { css, ThemeProvider } from 'styled-components';
+import Container from './style/container';
+
 function App() {
-  return <div className="App"></div>;
+  return (
+    <>
+      <GlobalStyle />
+      <ThemeProvider theme={theme}>
+        <AppContainer width="70vw" height="100vh">
+          <Header />
+          <Nav />
+          <MainContainer width="95%" height="75%">
+            <div>12312312312312312</div>
+          </MainContainer>
+        </AppContainer>
+      </ThemeProvider>
+    </>
+  );
 }
+
+const MainContainer = styled(Container)`
+  background: blue;
+  margin: 0 auto;
+  display: flex;
+`;
+
+const AppContainer = styled(Container)`
+  background-color: ${({ theme }) => theme.colors.grey};
+  margin: 0 auto;
+`;
 
 export default App;
