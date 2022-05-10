f1422c8c2e4878ab6e79fc6ed082a0cabfa8d7b1
jm <jm.dev.study@gmail.com>
2022-05-10 16:56:30 +0900

    Design: Order 컴포넌트의 스타일 수정 - styled-components의 네이밍 변경 - overflow 적용
    Related to: #4

--- src/components/VendingMachine/OrderContainer/Order.jsx
+++ src/components/VendingMachine/OrderContainer/Order.jsx
@@ -1,14 +1,25 @@
 import styled from 'styled-components';
 
 export default function UserOrder() {
-  return <Order />;
+  return <OrderList />;
 }
 
-const Order = styled.div`
+const OrderList = styled.ul`
   width: 100%;
   height: 400px;
   margin-bottom: 12px;
+  padding: 20px;
   border: 1px solid ${({ theme }) => theme.colors.gray3};
   border-radius: 12px;
+  overflow-y: auto;
   background: ${({ theme }) => theme.colors.offWhite};
 `;
+
+// const Order = styled.li`
+//   margin-bottom: 4px;
+//   ${({ theme }) => theme.fontStyles.xSmallRegular};
+
+//   strong {
+//     font-weight: 700;
+//   }
+// `;
