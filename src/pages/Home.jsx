220820b93622ed6fa8d12b0058a2db0b13dcf049
yongseongjeon <yongseong.jeon95@gmail.com>
2022-05-10 14:36:27 +0900

    💄 자판기 상품 UI 추가
    - Product 컴포넌트 생성
    - mock 데이터로 Product 랜더링

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,23 @@
+import Product from 'components/Product';
+import PRODUCTS from 'mock/products';
 import React from 'react';
+import styled from 'styled-components';
 
 function Home() {
-  return <div>This is a Home.</div>;
+  return (
+    <Products>
+      {PRODUCTS.map(({ name, price }) => (
+        <Product name={name} price={price} />
+      ))}
+    </Products>
+  );
 }
 
 export default Home;
+
+const Products = styled.div({
+  display: 'grid',
+  gridTemplateColumns: 'repeat(4, 100px)',
+  gap: '20px',
+  marginTop: '20px',
+});
