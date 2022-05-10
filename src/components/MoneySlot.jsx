d15a89d9cc0e26ea52b233c1e77cb00ec9bd5602
nanjeong <whdwkxj7@naver.com>
2022-05-10 16:16:24 +0900

    feat: 입력된 금액을 화폐 단위로 변경, 지갑에 남아 있는 돈 중에서 빠져나가도록 함

--- src/components/MoneySlot.jsx
+++ src/components/MoneySlot.jsx
@@ -1,12 +1,32 @@
+const getMoneyFromWallet = (input, wallet) => {
+    const unitOfMoney = Object.keys(wallet);
+    const difference = unitOfMoney.map((unit) => [
+        Math.abs(input - unit),
+        unit,
+    ]);
+    const minDifference = difference
+        .sort((a, b) => a[0] - b[0])
+        .filter(([_, unit]) => wallet[unit] > 0);
+
+    return minDifference.length ? minDifference[0][1] : null;
+};
+
 function MoneySlot({ addRecord, wallet }) {
     const putMoney = (event) => {
-        if (event.key === "Enter") {
-            event.preventDefault();
-            const money = event.target.value;
-            wallet[money] -= 1;
-            addRecord(`${money}원이 투입됨`);
-            event.target.value = "";
+        if (event.key !== "Enter") {
+            return;
         }
+
+        event.preventDefault();
+        const moneyInput = event.target.value;
+        event.target.value = "";
+
+        const moneyFromWallet = getMoneyFromWallet(moneyInput, wallet);
+        if (!moneyFromWallet) {
+            return;
+        }
+        wallet[moneyFromWallet] -= 1;
+        addRecord(`${moneyFromWallet}원이 투입됨`);
     };
 
     return (
