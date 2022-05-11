47d61830633078ddd9d6d1e2f443353981d65629
jindonyy <rsefa94@gmail.com>
2022-05-11 15:14:24 +0900

    feat: 라우터 생성
    closed issue #5

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,24 @@
 import React from 'react';
+import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
+import VendingMachine from 'pages/VendingMachine';
+import Wallet from 'pages/Wallet';
 import 'App.css';
 
 function App() {
-  return <div className="App" />;
+  return (
+    <div className="App">
+      <BrowserRouter>
+        <nav>
+          <Link to="/">자판기</Link>
+          <Link to="/wallet">지갑</Link>
+        </nav>
+        <Routes>
+          <Route path="/" element={<VendingMachine />} />
+          <Route path="/wallet" element={<Wallet />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
 }
 
 export default App;
---
+++ src/pages/VendingMachine.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function VendingMachine() {
+  return <div>자판기 페이지 입니다.</div>;
+}
+
+export default VendingMachine;
