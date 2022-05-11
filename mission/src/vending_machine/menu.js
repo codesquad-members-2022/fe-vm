cd0be3f624e416b260681775e567e40e17804104
onlinekkabchi <66970178+onlinekkabchi@users.noreply.github.com>
2022-05-11 22:09:58 +0900

    feat: PR 수요일 (#12)

---
+++ mission/src/vending_machine/menu.js
@@ -0,0 +1,104 @@
+const menu = [
+    {
+        name: "콜라",
+        price: 500,
+        amount: 5,
+    },
+    {
+        name: "사이다",
+        price: 1000,
+        amount: 4,
+    },
+    {
+        name: "파인애플맛 환타",
+        price: 400,
+        amount: 3,
+    },
+    {
+        name: "포도맛 환타",
+        price: 300,
+        amount: 2,
+    },
+    {
+        name: "레몬에이드",
+        price: 900,
+        amount: 1,
+    },
+    {
+        name: "봉봉",
+        price: 1000,
+        amount: 0,
+    },
+    {
+        name: "코코아주스",
+        price: 1000,
+        amount: 1,
+    },
+    {
+        name: "콜라제로",
+        price: 1000,
+        amount: 2,
+    },
+    {
+        name: "파워에이드",
+        price: 2000,
+        amount: 3,
+    },
+    {
+        name: "초코우유",
+        price: 1000,
+        amount: 4,
+    },
+    {
+        name: "초코우유2",
+        price: 7000,
+        amount: 5,
+    },
+    {
+        name: "초코우유3",
+        price: 600,
+        amount: 0,
+    },
+    {
+        name: "딸바주스",
+        price: 1000,
+        amount: 1,
+    },
+    {
+        name: "바나나우유",
+        price: 500,
+        amount: 2,
+    },
+    {
+        name: "커피우유",
+        price: 1000,
+        amount: 3,
+    },
+    {
+        name: "알로에",
+        price: 1200,
+        amount: 4,
+    },
+    {
+        name: "콘칩",
+        price: 1000,
+        amount: 5,
+    },
+    {
+        name: "새우깡",
+        price: 1000,
+        amount: 4,
+    },
+    {
+        name: "감자칩",
+        price: 2000,
+        amount: 3,
+    },
+    {
+        name: "칸쵸",
+        price: 1000,
+        amount: 2,
+    },
+];
+
+export { menu };
