884a23c0eac79edb29bfcae4fc48bea9d919cafc
youryu0212 <youryu0212@naver.com>
2022-05-11 11:40:55 +0900

    feat: ChargeForm에 지갑잔액, 자판기 투입금액 상태 반영

--- fe/src/Components/MoneyScreenBoard/ChargeForm.jsx
+++ fe/src/Components/MoneyScreenBoard/ChargeForm.jsx
@@ -1,4 +1,6 @@
-import { useState } from 'react';
+import { SetWalletMoneyContext, WalletMoneyContext } from 'App';
+import { InvestmentContext, SetInvestmentContext } from 'Pages/VendingMachine';
+import { useContext, useState } from 'react';
 import { CashInput, Button, ChargeForm } from './ChargeForm.styled';
 
 export default function ChargeScreen() {
@@ -6,14 +8,32 @@ export default function ChargeScreen() {
 
   const handleSubmit = (evt) => {
     evt.preventDefault();
-    console.log(cash);
-    // cash 사용
+    const coins = walletMoney.amount;
+    const adjustedCash = adjustCash(coins, cash);
+
+    if (!isReasonableCoin(walletMoney, adjustedCash)) {
+      return;
+    }
+
+    chargeCash({
+      investment,
+      setInvestment,
+      walletMoney,
+      setWalletMoney,
+      setCash,
+      adjustedCash,
+    });
   };
 
-  const handleKeyUpInput = (evt) => {
+  const handleInputChange = (evt) => {
     const { value } = evt.target;
     setCash(value);
   };
+
+  const walletMoney = useContext(WalletMoneyContext);
+  const setWalletMoney = useContext(SetWalletMoneyContext);
+  const investment = useContext(InvestmentContext);
+  const setInvestment = useContext(SetInvestmentContext);
 
   return (
     <ChargeForm flex onSubmit={handleSubmit}>
@@ -23,8 +43,9 @@ export default function ChargeScreen() {
         flex
         justify="center"
         align="center"
-        onKeyUp={handleKeyUpInput}
+        onChange={handleInputChange}
         autoFocus
+        value={cash || ''}
       ></CashInput>
       <Button type="submit" flex justify="center" align="center">
         클릭
@@ -32,3 +53,57 @@ export default function ChargeScreen() {
     </ChargeForm>
   );
 }
+
+const convertCoin = (replaceCoin, coinAndCount, cash) => {
+  const [coin, cnt] = coinAndCount.map(Number);
+  if (cnt === 0) {
+    return replaceCoin;
+  }
+  const moneyDiff = Math.abs(coin - cash);
+  if (replaceCoin.moneyDiff > moneyDiff) {
+    [replaceCoin.moneyDiff, replaceCoin.coin] = [moneyDiff, coin];
+  }
+  return replaceCoin;
+};
+
+const adjustCash = (coins, cash) => {
+  cash = Number(cash);
+
+  const { coin } = Object.entries(coins).reduce(
+    (replaceCoin, coinAndCount) => {
+      return convertCoin(replaceCoin, coinAndCount, cash);
+    },
+    {
+      moneyDiff: Infinity,
+      coin: 0,
+    },
+  );
+  return coin;
+};
+
+const isReasonableCoin = (walletMoney, cash) => {
+  cash = Number(cash);
+  if (cash === 0) {
+    return;
+  }
+  const coins = walletMoney.amount;
+  const reasonableCoin = Object.entries(coins).find((coin) => {
+    const [money, cnt] = coin.map(Number);
+    return money === cash && cnt > 0;
+  });
+  return reasonableCoin;
+};
+
+const chargeCash = ({ investment, setInvestment, walletMoney, setWalletMoney, setCash, adjustedCash }) => {
+  investment.amount += Number(adjustedCash);
+  const newInvestment = { ...investment };
+
+  if (walletMoney.amount[adjustedCash]) {
+    walletMoney.amount[adjustedCash] -= 1;
+  }
+  const newWalletMoney = { ...walletMoney };
+
+  setCash(adjustedCash);
+  setInvestment(newInvestment);
+  setWalletMoney(newWalletMoney);
+};
