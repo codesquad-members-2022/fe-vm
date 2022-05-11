f124896a289287a1b046683de2faa9c003ec57ec
youryu0212 <youryu0212@naver.com>
2022-05-11 15:08:23 +0900

    feat: 이벤트 발생 안내 메시지 스크린 컴포넌트 구현

---
+++ fe/src/Components/MessageScreen.styled.js
@@ -0,0 +1,19 @@
+import styled from 'styled-components';
+
+export const ScreenContainer = styled.div`
+  width: 1000px;
+  height: 500px;
+  border: 2px solid #3f51b5;
+  box-sizing: border-box;
+  overflow: auto;
+`;
+
+export const Screen = styled.div`
+  padding: 20px;
+`;
+
+export const Message = styled.div`
+  font-size: 1.25rem;
+  font-weight: 600;
+  margin-bottom: 10px;
+`;
