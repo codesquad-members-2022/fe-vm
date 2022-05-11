c2ecd5a225ae16b2f007a937950ecd4dd14dd9dd
godhyeongman <88314186+godhyeongman@users.noreply.github.com>
2022-05-11 14:37:32 +0900

    feat: 유저 계좌 고유 아이디 값, uuid 유틸 추가

--- src/mocks/MoneyButtonData.js
+++ src/mocks/MoneyButtonData.js
@@ -1,1 +1,11 @@
-export const MONEY_BUTTON_DATA = [10, 50, 100, 500, 1000, 5000, 10000];
+import {uuidv4} from '../Utils';
+
+export const MONEY_BUTTON_DATA = [
+  {unit: 10000, count: 0, id: uuidv4()},
+  {unit: 5000, count: 0, id: uuidv4()},
+  {unit: 1000, count: 0, id: uuidv4()},
+  {unit: 500, count: 0, id: uuidv4()},
+  {unit: 100, count: 0, id: uuidv4()},
+  {unit: 50, count: 0, id: uuidv4()},
+  {unit: 10, count: 0, id: uuidv4()},
+];
