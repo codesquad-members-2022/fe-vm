d2feb56d5f2666cf6ec646f91d3c634b05e304df
godhyeongman <aaaa4000@naver.com>
2022-05-11 16:03:19 +0900

    refactor: 지갑 컴포넌트 분리 필요없는 로직 삭제

---
+++ src/Component/Wallet/InsertButton.jsx
@@ -0,0 +1,67 @@
+import React, {useContext} from 'react';
+import styled from 'styled-components';
+
+import {UserAccount} from '../../Store';
+
+const divideCurrentMoney = (moneyList, money) => {
+  moneyList.forEach(v => {
+    v.count = parseInt(money / v.unit);
+    money = money % v.unit;
+  });
+  return moneyList;
+};
+
+const handleMoneyButton = (unit, currentMoney, dispatchCurrentMoney) => {
+  if (currentMoney < unit) {
+    return;
+  }
+
+  dispatchCurrentMoney({type: 'decrease', income: unit});
+};
+
+export const InsertButton = ({insertBtnData}) => {
+  const {currentMoney, dispatchCurrentMoney} = useContext(UserAccount);
+  const insertBtnList = divideCurrentMoney(insertBtnData, currentMoney);
+
+  return insertBtnList.map(({unit, id, count}) => (
+    <InsertBtnWrapper>
+      <MoneyBtn
+        key={id}
+        onClick={() =>
+          handleMoneyButton(unit, currentMoney, dispatchCurrentMoney)
+        }
+      >
+        {unit + '원'}
+      </MoneyBtn>
+      <MoneyBtn>{count + '개'}</MoneyBtn>
+    </InsertBtnWrapper>
+  ));
+};
+
+const MoneyBtn = styled.div`
+  width: 100px;
+  height: 100px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  border: 1px solid black;
+  margin-top: 20px;
+  margin-right: 10px;
+
+  cursor: pointer;
+  :hover {
+    background-color: rgba(0, 0, 0, 0.4);
+  }
+
+  :active {
+    background-color: rgba(0, 0, 0, 0.2);
+  }
+`;
+
+const InsertBtnWrapper = styled.div`
+  display: flex;
+
+  :last-child {
+    margin-right: none;
+  }
+`;
