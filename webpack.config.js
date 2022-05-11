90781232c648a3fb51f0f2f586cce273020ca294
muffin9 <jinlog9@gmail.com>
2022-05-11 14:43:59 +0800

    build: React + typescript 절대경로 설정

--- webpack.config.js
+++ webpack.config.js
@@ -11,6 +11,7 @@ module.exports = {
     app: path.join(__dirname, 'src', 'index.tsx'),
   },
   resolve: {
+    alias: { '@': path.resolve(__dirname, './src/') },
     extensions: ['.ts', '.tsx', '.js'],
   },
   module: {
