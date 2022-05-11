5a6424a134ebf88e98a7fc2ea90a3d7e0823eb35
otterp012 <otterp012@gmail.com>
2022-05-11 21:51:10 +0900

    [docs] : 상수 파일 추가

---
+++ src/constant/constant.js
@@ -0,0 +1,106 @@
+const DUMMY_DATA = [
+  {
+    id: 1,
+    name: 'coke',
+    price: 500,
+  },
+  {
+    id: 2,
+    name: 'cider',
+    price: 1000,
+  },
+  {
+    id: 3,
+    name: 'iced coffee',
+    price: 1000,
+  },
+  {
+    id: 4,
+    name: 'coffee',
+    price: 700,
+  },
+  {
+    id: 5,
+    name: 'iced water',
+    price: 500,
+  },
+  {
+    id: 6,
+    name: 'coke zero',
+    price: 1100,
+  },
+  {
+    id: 7,
+    name: 'milk',
+    price: 1000,
+  },
+  {
+    id: 8,
+    name: 'banana juice',
+    price: 1500,
+  },
+  {
+    id: 9,
+    name: 'tomato juice',
+    price: 1700,
+  },
+  {
+    id: 10,
+    name: 'water',
+    price: 2300,
+  },
+  {
+    id: 11,
+    name: 'dcf coffee',
+    price: 1800,
+  },
+  {
+    id: 12,
+    name: 'latte',
+    price: '3000',
+  },
+  {
+    id: 13,
+    name: 'sweets',
+    price: 2800,
+  },
+  {
+    id: 14,
+    name: 'candy',
+    price: 500,
+  },
+  {
+    id: 15,
+    name: 'pop',
+    price: 700,
+  },
+  {
+    id: 16,
+    name: 'snack',
+    price: 900,
+  },
+  {
+    id: 17,
+    name: 'apple',
+    price: 2300,
+  },
+  {
+    id: 18,
+    name: 'apple juice',
+    price: 3000,
+  },
+  {
+    id: 19,
+    name: 'ice',
+    price: 300,
+  },
+  {
+    id: 20,
+    name: 'icecream',
+    price: 3300,
+  },
+];
+
+const _ = undefined;
+
+export { DUMMY_DATA, _ };
