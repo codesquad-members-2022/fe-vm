ddb5c8cb68a13a9a69c859208f61135645068ab4
decay95 <decay95@naver.com>
2022-05-11 15:14:46 +0900

    [feat] 라우터 적용하여 재고 관리 화면, 자판기 화면 이동 구현

--- src/index.js
+++ src/index.js
@@ -1,13 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import App from './App';
+import { Store } from './components/main/store';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <BrowserRouter>
-      <App />
+      <Routes>
+        <Route path="/" element={<App />} />
+        <Route path="/store" element={<Store />} />
+      </Routes>
     </BrowserRouter>
   </React.StrictMode>
 );
