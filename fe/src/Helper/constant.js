651abf23a3e44b0f9af334540735079d0ef5fe53
youryu0212 <youryu0212@naver.com>
2022-05-11 11:26:14 +0900

    feat: 지갑잔액, 자판기에 투입된 금액 API 추가

--- fe/src/Helper/constant.js
+++ fe/src/Helper/constant.js
@@ -1,1 +1,5 @@
-export const DRINK_API = `http://localhost:3001/beverages`;
+const API_URL = 'http://localhost:3002';
+export const DRINK_API = `${API_URL}/beverages`;
+export const WALLET_API = `${API_URL}/money/0`;
+export const INVESTMENT_API = `${API_URL}/money/1`;
+export const COIN_LIST = [10000, 5000, 1000, 500, 100, 50, 10];
