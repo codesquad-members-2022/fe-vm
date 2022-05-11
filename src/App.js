8c2ed6dc947475d3449c9f4c17c28c882fb0a89c
ldldz <qkrqudrjs4350@gmail.com>
2022-05-11 15:09:02 +0900

    Refact: index.js jsx App.js 로 이동

--- src/App.js
+++ src/App.js
@@ -1,9 +1,23 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import GlobalStyles from "./GlobalStyles";
 import VendingMachine from "./routes/VendingMachine";
+import Wallet from "./routes/Wallet";
+import Nav from "./components/Nav";
+import { WalletProvider } from "./contexts/WalletContext";
 
 export default function App() {
   return (
     <div className="App">
-      <VendingMachine />
+      <GlobalStyles></GlobalStyles>
+      <BrowserRouter>
+        <WalletProvider>
+          <Nav />
+          <Routes>
+            <Route path="/" element={<VendingMachine />}></Route>
+            <Route path="/wallet" element={<Wallet />}></Route>
+          </Routes>
+        </WalletProvider>
+      </BrowserRouter>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,22 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
 import App from "./App";
-import Nav from "./components/Nav";
-import { WalletProvider } from "./contexts/WalletContext";
-import Wallet from "./routes/Wallet";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
-      <WalletProvider>
-        <Nav />
-        <Routes>
-          <Route path="/" element={<App />}></Route>
-          <Route path="/wallet" element={<Wallet />}></Route>
-        </Routes>
-      </WalletProvider>
-    </BrowserRouter>
+    <App />
   </React.StrictMode>
 );
