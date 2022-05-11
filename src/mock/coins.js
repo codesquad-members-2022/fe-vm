00b33fc5ad4aed5cc3eb4812fe17b0a395127946
yongseongjeon <yongseong.jeon95@gmail.com>
2022-05-11 15:28:36 +0900

    ✨ 지갑에 있는 돈의 갯수 랜덤으로 지정

--- src/mock/coins.js
+++ src/mock/coins.js
@@ -1,11 +1,19 @@
 const COINS = [
-  { AMOUNT: 10, CNT: 10 },
-  { AMOUNT: 50, CNT: 10 },
-  { AMOUNT: 100, CNT: 10 },
-  { AMOUNT: 500, CNT: 10 },
-  { AMOUNT: 1000, CNT: 10 },
-  { AMOUNT: 5000, CNT: 10 },
-  { AMOUNT: 10000, CNT: 10 },
+  { AMOUNT: 10, CNT: extractNumber1to10() },
+  { AMOUNT: 50, CNT: extractNumber1to10() },
+  { AMOUNT: 100, CNT: extractNumber1to10() },
+  { AMOUNT: 500, CNT: extractNumber1to10() },
+  { AMOUNT: 1000, CNT: extractNumber1to10() },
+  { AMOUNT: 5000, CNT: extractNumber1to10() },
+  { AMOUNT: 10000, CNT: extractNumber1to10() },
 ];
+
+function extractNumber1to10() {
+  return getRandomNumber({ min: 0, max: 10 });
+}
+
+function getRandomNumber({ min, max }) {
+  return Math.floor(min + Math.random() * (max - min + 1));
+}
 
 export default COINS;
