f3fdc0b954bb62abec8c720d491a94775637cc39
youryu0212 <youryu0212@naver.com>
2022-05-11 11:39:15 +0900

    feat: Beverage 컴포넌트에 투입금액 상태 반영

--- fe/src/Components/ProductScreenBoard/Beverage.jsx
+++ fe/src/Components/ProductScreenBoard/Beverage.jsx
@@ -1,8 +1,25 @@
+import { InvestmentContext, SetInvestmentContext } from 'Pages/VendingMachine';
+import { useContext } from 'react';
 import { BeverageContainer, Price, Title } from './Beverage.styled';
 
 export default function Beverage({ title, price }) {
+  const setInvestment = useContext(SetInvestmentContext);
+  const investment = useContext(InvestmentContext);
+  const investmentPrice = (investment && investment.amount) || 0;
+  const buyPossible = price <= investmentPrice;
+
+  const handleOrder = () => {
+    if (!buyPossible) {
+      return;
+    }
+    const remainMoney = investmentPrice - price;
+    investment.amount = remainMoney;
+    const newInvestment = { ...investment };
+    setInvestment(newInvestment);
+  };
+
   return (
-    <BeverageContainer>
+    <BeverageContainer buyPossible={buyPossible} onClick={handleOrder}>
       <Title flex justify="center" align="center">
         {title}
       </Title>
