831dfb3b7b0c9206b9d5025c7f61c72722e85655
Cola <bcad1591@gmail.com>
2022-05-11 15:07:34 +0900

    Feat: InputController 구조 및 스타일 추가

--- src/components/InputController/index.jsx
+++ src/components/InputController/index.jsx
@@ -1,7 +1,52 @@
 import React from 'react';
+import styled from 'styled-components';
+
+const InputControllerLayout = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 99%;
+  justify-content: space-around;
+`;
+
+const InputLayer = styled.div`
+  display: flex;
+  align-items: center;
+  font-size: 20px;
+`;
+
+const Input = styled.input.attrs({ type: 'text', placeholder: '0' })`
+  font-size: 20px;
+  width: 100%;
+  text-align: right;
+  border-bottom: 1px solid black;
+`;
+
+const ReturnButton = styled.button.attrs({ type: 'button' })`
+  font-size: 20px;
+  padding: 5px;
+  border: 1px solid black;
+  cursor: pointer;
+  transition: background-color 100ms;
+
+  &:hover {
+    background-color: #00000022;
+  }
+
+  &:active {
+    background-color: #00000011;
+  }
+`;
 
 const InputController = ({ className }) => {
-  return <div className={className}>InputController</div>;
+  return (
+    <InputControllerLayout className={className}>
+      <InputLayer>
+        <Input />
+        <span>원</span>
+      </InputLayer>
+      <ReturnButton>반환</ReturnButton>
+    </InputControllerLayout>
+  );
 };
 
 export default InputController;
