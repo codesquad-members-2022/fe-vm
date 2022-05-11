6d68bf5d01a07e602090f65f0e51e5d727b85c63
youryu0212 <youryu0212@naver.com>
2022-05-11 15:05:52 +0900

    feat: 음료를 주문했을 때 이벤트 알림 상태에 반영

--- fe/src/Components/ProductScreenBoard/Beverage.jsx
+++ fe/src/Components/ProductScreenBoard/Beverage.jsx
@@ -1,10 +1,13 @@
-import { InvestmentContext, SetInvestmentContext } from 'Pages/VendingMachine';
+import { initAlertMessage } from 'Helper/constant';
+import { InvestmentContext, SetAlertMessage, SetInvestmentContext } from 'Pages/VendingMachine';
 import { useContext } from 'react';
 import { BeverageContainer, Price, Title } from './Beverage.styled';
 
 export default function Beverage({ title, price }) {
   const setInvestment = useContext(SetInvestmentContext);
   const investment = useContext(InvestmentContext);
+  const setAlertMessage = useContext(SetAlertMessage);
+
   const investmentPrice = (investment && investment.amount) || 0;
   const buyPossible = price <= investmentPrice;
 
@@ -16,6 +19,7 @@ export default function Beverage({ title, price }) {
     investment.amount = remainMoney;
     const newInvestment = { ...investment };
     setInvestment(newInvestment);
+    alertOrderMessage({ title, setAlertMessage });
   };
 
   return (
@@ -29,3 +33,9 @@ export default function Beverage({ title, price }) {
     </BeverageContainer>
   );
 }
+
+const alertOrderMessage = ({ title, setAlertMessage }) => {
+  const alertMessage = { ...initAlertMessage };
+  alertMessage.orderTitle = title;
+  setAlertMessage(alertMessage);
+};
