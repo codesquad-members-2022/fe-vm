5a6a0a4f64fcf268892f74a6056360e5e241952e
ju-kkim <wndus8198@naver.com>
2022-05-11 14:56:24 +0900

    feat: App.js Header컴포넌트 추가

--- src/App.js
+++ src/App.js
@@ -1,5 +1,12 @@
+import React from 'react'
+import Header from './Components/Header'
+
 function App() {
-  return <div className="App">Vending Machine</div>
+  return (
+    <>
+      <Header />
+    </>
+  )
 }
 
 export default App
