2f4841420eab8413090e8b61837eb358b24cc7f2
BB-choi <choibobae0809@gmail.com>
2022-05-11 16:45:38 +0900

    fix: 잘못설정되어 있던 스타일 정정
    - 불필요한 string 삭제

--- src/components/VendingMachine/DispenseArea/DispenseArea.styled.js
+++ src/components/VendingMachine/DispenseArea/DispenseArea.styled.js
@@ -5,7 +5,7 @@ const returnButtonStyle = {
   size: { width: "100%", height: "3rem" },
   fontStyle: theme.fontStyles.buttons.large,
   margin: `${theme.whitespace.default} 0`,
-  bgColor: `${theme.colors.darkblue}`,
+  bgColor: theme.colors.darkblue,
 };
 
 const DispenseAreaWrap = styled.div`
