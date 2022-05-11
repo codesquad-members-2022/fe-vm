7e9e3e61231d1dab8c01c05e8e6fef9bf7a24632
decay95 <decay95@naver.com>
2022-05-11 15:17:13 +0900

    [feat] header Style 및 토글 기능 구현

--- src/App.js
+++ src/App.js
@@ -1,17 +1,20 @@
 import { useState } from 'react';
+import GlobalStlyes from './Assets/GlobalStyles';
 import { Header } from './components/header/header';
+import { Main } from './components/main/main';
 import './App.css';
 
 function App() {
   const [isVMClicked, setVMClicked] = useState(true);
-  const [isWalletClicked, setWalletClicked] = useState(true);
-
+  const [isWalletClicked, setWalletClicked] = useState(false);
   return (
     <>
+      <GlobalStlyes />
       <Header
         VMStateObj={{ isVMClicked, setVMClicked }}
         WalletStateObj={{ isWalletClicked, setWalletClicked }}
       />
+      <Main isVMClicked={isVMClicked} isWalletClicked={isWalletClicked} />
     </>
   );
 }
