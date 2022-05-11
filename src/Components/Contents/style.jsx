46fd15075ea492e4f85fe9968214df79166fed57
ju-kkim <wndus8198@naver.com>
2022-05-11 16:01:21 +0900

    feat: Contents 공동 스타일 추가

---
+++ src/Components/Contents/style.jsx
@@ -0,0 +1,10 @@
+import styled from 'styled-components'
+import { Color, Radius20 } from '../../Assets/Common.style'
+
+export const ContentBox = styled.section`
+  width: 1000px;
+  margin: 0 auto;
+  padding: 45px 30px;
+  ${Radius20}
+  background: ${Color.ORANGE[100]};
+`
