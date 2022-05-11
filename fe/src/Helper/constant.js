6307216e579eef19dabb4c4833cc543bcd685315
youryu0212 <youryu0212@naver.com>
2022-05-11 15:02:15 +0900

    feat: 자판기 컴포넌트에 이벤트 알림 상태 추가

--- fe/src/Helper/constant.js
+++ fe/src/Helper/constant.js
@@ -3,3 +3,9 @@ export const DRINK_API = `${API_URL}/beverages`;
 export const WALLET_API = `${API_URL}/money/0`;
 export const INVESTMENT_API = `${API_URL}/money/1`;
 export const COIN_LIST = [10000, 5000, 1000, 500, 100, 50, 10];
+
+export const initAlertMessage = {
+  chargeCash: 0,
+  changeAmount: 0,
+  orderTitle: '',
+};
