1e718b7d131d5b6af4a1500d8c67d49d7c9a15b5
Millie <jay.lee.2280@gmail.com>
2022-05-11 19:59:50 +0900

    Design: set ThemeProvider & GlobalStyle
    * install 'styled-reset' package and apply to GlobalStyle
    * apply font

--- src/index.js
+++ src/index.js
@@ -1,11 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { ThemeProvider } from 'styled-components';
 
 import App from '@/App';
+import GlobalStyle from '@/styles/GlobalStyle';
+import { theme } from '@/styles/theme';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider theme={theme}>
+      <GlobalStyle />
+      <App />
+    </ThemeProvider>
   </React.StrictMode>,
 );
---
+++ src/styles/theme.js
@@ -0,0 +1,32 @@
+const greyScale = {
+  black: '#1b1b1b',
+  soldOut: '#777777',
+  primaryGrey: '#c4c4c4',
+  innerBackground: '#e5e5e5',
+  background: '#f4f4f4',
+  white: '#fff',
+};
+
+const color = {
+  yellow: '#F4FF77',
+  blue: '#2B73FF',
+  orange: '#FC6815',
+  red: '#ED5151',
+  green: '#36D21D',
+  purple: '#8228FF',
+  pink: '#FF4ACC',
+};
+
+const fontSize = {
+  xLarge: '50px',
+  large: '24px',
+  medium: '20px',
+  small: '16px',
+  xSmall: '14px',
+};
+
+export const theme = {
+  greyScale,
+  color,
+  fontSize,
+};
