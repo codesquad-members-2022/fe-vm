3e0ac61677a87ab96e680ebbf1f29f8ee581c97b
otterp012 <otterp012@gmail.com>
2022-05-12 11:57:31 +0900

    [fix]: PR리뷰 사항 수정

--- src/components/layout/Nav/Nav.js
+++ src/components/layout/Nav/Nav.js
@@ -2,6 +2,7 @@ import React from 'react';
 import styled from 'styled-components';
 import Button from '../../UI/Button';
 import { _ } from '../../../constant/constant';
+
 const Nav = (props) => {
   return (
     <StyledNav>
