265f63bc7e8912031d7d2d40e74c7dee136e11d9
mina-gwak <mina.gwak@kakao.com>
2022-05-13 06:02:28 +0900

    feat: Add styled tab menu (#6)

---
+++ src/components/molecules/TabMenu/TabMenu.style.js
@@ -0,0 +1,10 @@
+import styled from 'styled-components';
+
+const TabMenu = styled.div`
+  display: flex;
+  width: fit-content;
+  border-radius: 5px;
+  overflow: hidden;
+`;
+
+export { TabMenu };
