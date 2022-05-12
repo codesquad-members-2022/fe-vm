5e8b6f04795aed6190fb23ef7e56bfc492afe448
Muffin <jinlog9@gmail.com>
2022-05-13 08:03:00 +0900

    feat: 자판기 물품 관련 컴포넌트 작업진행중

---
+++ src/Components/Machine/Item.tsx
@@ -0,0 +1,89 @@
+import styled, { css } from 'styled-components';
+import { IItem } from '@/Context/ItemContext';
+
+interface PropsType {
+  item: IItem;
+  available: boolean;
+  handleImageClick: Function;
+}
+
+interface TStyledView {
+  available: boolean;
+  item: IItem;
+}
+
+const ItemWrapper = styled.div<TStyledView>`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  background-color: ${props => {
+    if (props.item.count === 0) {
+      return css`
+        ${({ theme }) => theme.colors.gray1}
+      `;
+    } else {
+      return css`
+        ${({ theme }) => theme.colors.white}
+      `;
+    }
+  }};
+
+  div {
+    cursor: pointer;
+    img {
+      width: 150px;
+      height: 100px;
+      content: ${props => {
+        if (props.item.count === 0) {
+          return css`url('images/soldout.png')`;
+        } else {
+          return css`url(${props.item.image})`;
+        }
+      }};
+      border: ${props => {
+        if (props.available)
+          return css`5px solid ${({ theme }) => theme.colors.ultramarine}`;
+      }};
+    }
+  }
+`;
+
+const InfoWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  margin-top: 10px;
+
+  span {
+    display: inline-block;
+    margin: 5px 0;
+  }
+`;
+
+export default function Item({
+  item,
+  available,
+  handleImageClick,
+}: PropsType): JSX.Element {
+  const { text, price, count } = item;
+  return (
+    <>
+      <ItemWrapper
+        item={item}
+        available={available}
+        onClick={() => handleImageClick(item, available)}
+      >
+        <div>
+          <img />
+        </div>
+        <InfoWrapper>
+          <span>{text}</span>
+          <span>가격: {price}</span>
+          <span>잔여수량: {count}개</span>
+        </InfoWrapper>
+      </ItemWrapper>
+    </>
+  );
+}
---
+++ src/Components/Machine/ItemList.tsx
@@ -0,0 +1,65 @@
+import styled from 'styled-components';
+import Item from '@/Components/Machine/Item';
+
+import { IItem, useItemState } from '@/Context/ItemContext';
+import { useMachineState } from '@/Context/MachineContext';
+
+const ItemListerWrapper = styled.article`
+  display: grid;
+  grid-template-columns: 1fr 1fr 1fr 1fr;
+  padding: 20px;
+  grid-gap: 20px;
+  width: 70%;
+  height: 100%;
+  border: 5px solid ${({ theme }) => theme.colors.ultramarine};
+  background-color: ${({ theme }) => theme.colors.white};
+  border-radius: 20px 0 0 20px;
+`;
+
+export default function ItemList(): JSX.Element {
+  const itemState = useItemState();
+  const machineState = useMachineState();
+
+  const machinePriceAmount = machineState.reduce((prev, cur) => {
+    return (prev += cur.unit);
+  }, 0);
+
+  const availablePurchaseItem = (price: number, count: number) => {
+    return count && machinePriceAmount > price ? true : false;
+  };
+
+  const handleImageClick = (item: IItem, available: boolean) => {
+    if (item.count <= 0) {
+      alert('매진된 상품입니다.');
+      return;
+    }
+
+    if (!available) {
+      return;
+    }
+
+    // MachineMessage Update
+    // 잔돈 -> Wallet Update
+    // 5초 카운트 종료
+    // 2초 뒤 상품 배출
+  };
+
+  return (
+    <>
+      <ItemListerWrapper>
+        <>
+          {itemState.map(item => {
+            return (
+              <Item
+                key={item.uuid}
+                item={item}
+                available={availablePurchaseItem(item.price, item.count)}
+                handleImageClick={handleImageClick}
+              />
+            );
+          })}
+        </>
+      </ItemListerWrapper>
+    </>
+  );
+}
