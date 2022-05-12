3f97506ffa7e4fd5642aee0280a354558769f57a
Sangbeom Heo <mansaout@gmail.com>
2022-05-12 14:29:06 +0900

    feat: 라우팅 설정 (#4)
    Layout
    ㄴ VendingMachine
    ㄴ Wallet
    ㄴ Admin
    NotFound

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+
+import Layout from 'components/layout/Layout';
+import { VendingMachine, Wallet, Admin, NotFound } from 'components/pages';
 
 function App() {
   return (
-    <>
-      <div className='App'>
-      </div>
-    </>
+    <div className='App'>
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<Layout />}>
+            <Route index element={<VendingMachine />} />
+            <Route path='vendingMachine' element={<VendingMachine />} />
+            <Route path='wallet' element={<Wallet />} />
+            <Route path='admin' element={<Admin />} />
+          </Route>
+          <Route path='*' element={<NotFound />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
   );
 }
 
