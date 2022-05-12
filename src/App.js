7a3f9b2895e75e42e7b53dfe669faa896f432e0a
Sangbeom Heo <mansaout@gmail.com>
2022-05-13 06:00:35 +0900

    feat: Route에서 path가 '/' 일 경우 'vendingMachine'으로 리다이렉트 (#8)

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom';
 
 import GlobalStyle from 'styles/GlobalStyle';
 
+import { CashProvider } from 'context';
 import { PATH } from 'constants';
 import Layout from 'components/layout/Layout';
 import { VendingMachine, Wallet, Admin, NotFound } from 'components/pages';
@@ -10,9 +11,14 @@ function App() {
   return (
     <div className='App'>
       <GlobalStyle />
+      <CashProvider>
         <BrowserRouter>
           <Routes>
             <Route path={URL.INDEX} element={<Layout />}>
+              <Route
+                index
+                element={<Navigate to={PATH.VENDING_MACHINE} replace />}
+              />
               <Route path={PATH.VENDING_MACHINE} element={<VendingMachine />} />
               <Route path={PATH.WALLET} element={<Wallet />} />
               <Route path={PATH.ADMIN} element={<Admin />} />
@@ -20,6 +26,7 @@ function App() {
             <Route path={PATH.ALL} element={<NotFound />} />
           </Routes>
         </BrowserRouter>
+      </CashProvider>
     </div>
   );
 }
