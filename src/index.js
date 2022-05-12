7c1592ae1978f4dd4e8c834e9e3351ae6e312bbf
Hemudi <ksum1205@naver.com>
2022-05-12 23:41:49 +0900

    Feat: Global Style과 Theme 구현
    Related to: #2

--- src/index.js
+++ src/index.js
@@ -1,10 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App';
+import App from 'App';
+import { ThemeProvider } from 'styled-components';
+import { defaultTheme } from 'style/default';
+import GlobalStyle from 'style/global';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider theme={defaultTheme}>
+      <GlobalStyle />
+      <App />
+    </ThemeProvider>
   </React.StrictMode>
 );
---
+++ src/style/default.js
@@ -0,0 +1,37 @@
+const fontSize = {
+  display: '40px',
+  xLarge: '24px',
+  large: '20px',
+  medium: '16px',
+  small: '14px',
+  xSmall: '12px',
+};
+
+const fontWeight = {
+  display: 700,
+  bold: 500,
+  regular: 400,
+};
+
+const colors = {
+  black: '#1B1B1B',
+  grey1: '#3F3F3F',
+  grey2: '#777777',
+  grey3: '#BCBCBC',
+  grey4: '#EBEBEB',
+  offWhite: '#F8F7F7',
+  white: '#FFFFFF',
+  darkBlue: '#013365',
+  skyBlue: '#97CBE8',
+  purple: '#A8226E',
+  green: '#34A840',
+  brightGreen: '#33FF00',
+  red: '#C45454',
+  darkRed: '#913B3B',
+};
+
+export const defaultTheme = {
+  fontSize,
+  fontWeight,
+  colors,
+};
