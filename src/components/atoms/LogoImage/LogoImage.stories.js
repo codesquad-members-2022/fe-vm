a15a6fdff17480fc293a73a5d4ea0391362f3897
Hemudi <ksum1205@naver.com>
2022-05-13 06:05:08 +0900

    Test: LogoImage Atom Component 스토리북 테스트 코드 작성
    * 좌우 반전 테스트
    
    Related to: #1

---
+++ src/components/atoms/LogoImage/LogoImage.stories.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import LogoImage from './LogoImage';
+
+export default {
+  title: 'Atoms/LogoImage',
+  component: LogoImage,
+};
+
+const Template = args => <LogoImage {...args} />;
+
+export const FlippedLogoImage = Template.bind({});
+FlippedLogoImage.args = {
+  isFlipped: true,
+};
+
+export const DefaultLogoImage = () => <LogoImage />;
