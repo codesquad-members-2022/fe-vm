36b9046a3d6d8f6ddc54ade1e4531d26d274efb9
Sangbeom Heo <mansaout@gmail.com>
2022-05-13 05:59:51 +0900

    refactor: Route path를 기존 문자열에서 상수로 변경해 관리 (#8)

--- src/App.js
+++ src/App.js
@@ -1,24 +1,25 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 import GlobalStyle from 'styles/GlobalStyle';
+
+import { PATH } from 'constants';
 import Layout from 'components/layout/Layout';
 import { VendingMachine, Wallet, Admin, NotFound } from 'components/pages';
 
 function App() {
   return (
     <div className='App'>
-      <BrowserRouter>
-        <Routes>
-          <Route path='/' element={<Layout />}>
-            <Route index element={<VendingMachine />} />
-            <Route path='vendingMachine' element={<VendingMachine />} />
-            <Route path='wallet' element={<Wallet />} />
-            <Route path='admin' element={<Admin />} />
-          </Route>
-          <Route path='*' element={<NotFound />} />
-        </Routes>
-      </BrowserRouter>
       <GlobalStyle />
+        <BrowserRouter>
+          <Routes>
+            <Route path={URL.INDEX} element={<Layout />}>
+              <Route path={PATH.VENDING_MACHINE} element={<VendingMachine />} />
+              <Route path={PATH.WALLET} element={<Wallet />} />
+              <Route path={PATH.ADMIN} element={<Admin />} />
+            </Route>
+            <Route path={PATH.ALL} element={<NotFound />} />
+          </Routes>
+        </BrowserRouter>
     </div>
   );
 }
