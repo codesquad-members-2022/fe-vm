046064c876b1a81ee51549d5d6f6aff20005d32f
khj <hongjung.kim.dev@gmail.com>
2022-05-13 00:36:19 +0900

    feat: wallet페이지 및 item 추가

--- src/pages/Wallet.jsx
+++ src/pages/Wallet.jsx
@@ -1,5 +1,41 @@
 import React from 'react';
 
+import WalletItem from '../components/WalletItem';
+import money from '../store/store';
+
 export default function Wallet() {
-  return <h1>지갑 페이지</h1>;
+  const handleClickWon = () => {
+    // Todo : store의 setPrice(won)
+  };
+
+  return (
+    <>
+      <ul>
+        {money.map(({ won, num }) => (
+          <WalletItem
+            key={`money-${won}`}
+            icon={`${won}원 `}
+            won={won}
+            num={num}
+            onClick={handleClickWon}
+          />
+        ))}
+      </ul>
+      <span>
+        {`${`${money
+          .map(({ won, num }) => won * num)
+          .reduce((aMoney, bMoney) => aMoney + bMoney)}`
+          .split('')
+          .reverse()
+          .map((element, index, array) => {
+            if ((index + 1) % 3 === 0 && index + 1 < array.length) {
+              return `,${element}`;
+            }
+            return element;
+          })
+          .reverse()
+          .join('')}원`}
+      </span>
+    </>
+  );
 }
---
+++ src/store/store.js
@@ -0,0 +1,11 @@
+const money = [
+  { won: 10, num: 0 },
+  { won: 50, num: 1 },
+  { won: 100, num: 5 },
+  { won: 500, num: 5 },
+  { won: 1000, num: 2 },
+  { won: 5000, num: 2 },
+  { won: 10000, num: 1 },
+];
+
+export default money;
