79972f89d9614f239720ba052b3968f563a4e961
Muffin <jinlog9@gmail.com>
2022-05-13 08:02:22 +0900

    feat: 자판기 오른쪽 영역 UI 작업

---
+++ src/Components/Machine/InfoList.tsx
@@ -0,0 +1,27 @@
+import styled from 'styled-components';
+import InputMoney from '@/Components/Machine/InputMoney';
+import ReturnMoney from '@/Components/Machine/ReturnMoney';
+import BoardMessage from '@/Components/Machine/BoardMessage';
+
+const InfoListWrapper = styled.section`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: space-between;
+  width: 30%;
+  background-color: ${({ theme }) => theme.colors.ultramarine};
+  border-radius: 0 20px 20px 0;
+  padding: 20px;
+`;
+
+export default function InfoList(): JSX.Element {
+  return (
+    <>
+      <InfoListWrapper>
+        <InputMoney />
+        <ReturnMoney />
+        <BoardMessage />
+      </InfoListWrapper>
+    </>
+  );
+}
