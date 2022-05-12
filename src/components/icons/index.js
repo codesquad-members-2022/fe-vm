ed12de75ad1239f2fe6936098a14ebfa9d5b8a36
Sangbeom Heo <mansaout@gmail.com>
2022-05-13 06:21:26 +0900

    feat: +,- 아이콘 추가 (#7)
    아이콘이미지, 아이콘타입, 아이콘 컴포넌트 추가

---
+++ src/components/icons/index.js
@@ -0,0 +1,4 @@
+import { ReactComponent as PlusIcon } from '../../asset/icons/plus.svg';
+import { ReactComponent as MinusIcon } from '../../asset/icons/minus.svg';
+
+export { PlusIcon, MinusIcon };
