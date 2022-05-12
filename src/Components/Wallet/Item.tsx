b642d50aef3e8120d7804f9e55e1ebd964c9d4c1
Muffin <jinlog9@gmail.com>
2022-05-13 07:59:30 +0900

    feat: 지갑 컴포넌트 기능 구현

---
+++ src/Components/Wallet/Item.tsx
@@ -0,0 +1,58 @@
+import styled, { css } from 'styled-components';
+import { useWalletDispatch } from '@/Context/WalletContext';
+import { useMachineDispatch } from '@/Context/MachineContext';
+
+interface TStyledView {
+  uuid?: number;
+}
+
+interface ItemType {
+  unit: number;
+  count: number;
+}
+
+const ItemComponent = styled.div<TStyledView>`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  border-radius: 8px;
+  border: 2px solid ${({ theme }) => theme.colors.ultramarine};
+  cursor: pointer;
+
+  &:hover {
+    ${props => {
+      if (props.uuid) {
+        return css`
+          opacity: 0.5;
+        `;
+      }
+    }}
+  }
+`;
+
+export default function Item({ unit, count }: ItemType): JSX.Element {
+  const walletDispatch = useWalletDispatch();
+  const machineDispatch = useMachineDispatch();
+
+  const decreaseUnitCount = (unit: number, count: number) =>
+    walletDispatch({ type: 'DECREASE_WALLET_UNIT', unit, count });
+
+  const notifyUnitMessage = (unit: number, message: string) => {
+    machineDispatch({ type: 'INSERT_MESSAGE', unit, message });
+  };
+
+  const handleUnitClick = (unit: number, count: number) => {
+    if (count <= 0) return;
+    decreaseUnitCount(unit, 1);
+    notifyUnitMessage(unit, `${unit}원이 투입됐음`);
+  };
+
+  return (
+    <>
+      <ItemComponent uuid={unit} onClick={() => handleUnitClick(unit, count)}>
+        {unit}원
+      </ItemComponent>
+      <ItemComponent>{count}개</ItemComponent>
+    </>
+  );
+}
---
+++ src/Components/Wallet/index.tsx
@@ -0,0 +1,62 @@
+import styled from 'styled-components';
+import Item from '@/Components/Wallet/Item';
+import { useWalletState, useWalletDispatch } from '@/Context/WalletContext';
+
+interface TStyledView {
+  children: JSX.Element | JSX.Element[];
+}
+
+const WalletGridWrapper = styled.section<TStyledView>`
+  display: grid;
+  grid-template-columns: 1fr 1fr;
+  padding: 20px 10px;
+  grid-gap: 20px;
+  width: 40%;
+  height: 700px;
+  margin: 0 auto;
+  border-radius: 20px;
+  border: 5px solid ${({ theme }) => theme.colors.ultramarine};
+`;
+
+const WalletSumComponent = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  border-radius: 8px;
+  border: 2px solid ${({ theme }) => theme.colors.ultramarine};
+`;
+
+export default function Wallet(): JSX.Element {
+  const walletState = useWalletState();
+  const walletDispatch = useWalletDispatch();
+
+  const increaseUnitCount = (unit: number, count: number) =>
+    walletDispatch({ type: 'INCREASE_WALLET_UNIT', unit, count });
+
+  const decreaseUnitCount = (unit: number, count: number) =>
+    walletDispatch({ type: 'DECREASE_WALLET_UNIT', unit, count });
+
+  let sum = 0;
+
+  return (
+    <>
+      <button onClick={() => increaseUnitCount(10, 1)}>
+        Unit Increase Count Button
+      </button>
+      <button onClick={() => decreaseUnitCount(10, 1)}>
+        Unit Decrease Count Button
+      </button>
+      <WalletGridWrapper>
+        <>
+          {walletState.map(wallet => {
+            sum += wallet.unit * wallet.count;
+            return (
+              <Item key={wallet.uuid} unit={wallet.unit} count={wallet.count} />
+            );
+          })}
+        </>
+        <WalletSumComponent>{sum}원</WalletSumComponent>
+      </WalletGridWrapper>
+    </>
+  );
+}
