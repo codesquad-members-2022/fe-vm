a7a7d76f2c30273a28d4af248181738ab813d917
SeungHyun <fm10033@gmail.com>
2022-05-12 14:15:14 +0900

    Feat : product, money 데이터 구현

---
+++ vending-machine/src/datas/money.js
@@ -0,0 +1,34 @@
+const wallet = [
+  {
+    money: 10,
+    amount: 2,
+  },
+  {
+    money: 50,
+    amount: 2,
+  },
+  {
+    money: 100,
+    amount: 4,
+  },
+  {
+    money: 500,
+    amount: 5,
+  },
+  {
+    money: 1000,
+    amount: 2,
+  },
+  {
+    money: 5000,
+    amount: 1,
+  },
+  {
+    money: 10000,
+    amount: 4,
+  },
+  {
+    money: 50000,
+    amount: 0,
+  },
+];
---
+++ vending-machine/src/datas/products.js
@@ -0,0 +1,72 @@
+const product = [
+  {
+    title: "콜라",
+    price: 500,
+    amount: 3,
+  },
+  {
+    title: "사이다",
+    price: 1000,
+    amount: 2,
+  },
+  {
+    title: "파인애플맛 환타",
+    price: 400,
+    amount: 0,
+  },
+  {
+    title: "포도맛 환타",
+    price: 300,
+    amount: 0,
+  },
+  {
+    title: "레몬에이드",
+    price: 900,
+    amount: 5,
+  },
+  {
+    title: "봉봉",
+    price: 1200,
+    amount: 6,
+  },
+  {
+    title: "코코아",
+    price: 300,
+    amount: 7,
+  },
+  {
+    title: "코카콜라 제로",
+    price: 500,
+    amount: 2,
+  },
+  {
+    title: "파워에이드",
+    price: 700,
+    amount: 4,
+  },
+  {
+    title: "초코우유",
+    price: 1000,
+    amount: 5,
+  },
+  {
+    title: "딸기우유",
+    price: 1000,
+    amount: 3,
+  },
+  {
+    title: "커피우유",
+    price: 400,
+    amount: 5,
+  },
+  {
+    title: "데자와",
+    price: 1000,
+    amount: 100,
+  },
+  {
+    title: "빅웨이브",
+    price: 1500,
+    amount: 4,
+  },
+];
