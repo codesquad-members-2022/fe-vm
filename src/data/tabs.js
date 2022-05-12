42bd45db618c0badd15fde3499933a9f732eba11
mina-gwak <mina.gwak@kakao.com>
2022-05-13 06:01:38 +0900

    feat: Create temporary tab data (#6)

---
+++ src/data/tabs.js
@@ -0,0 +1,16 @@
+const tabs = [
+  {
+    id: 1,
+    content: '자판기',
+    path: '/',
+    $active: true,
+  },
+  {
+    id: 2,
+    content: '지갑',
+    path: '/wallet',
+    $active: false,
+  },
+];
+
+export default tabs;
