979682f231bc8042ceed253e3be59233b70d0a38
jindonyy <rsefa94@gmail.com>
2022-05-12 23:06:34 +0900

    Feat: 자판기 금액 입력 기능 구현
    issue #8

--- src/components/orderArea/MoneySlot.jsx
+++ src/components/orderArea/MoneySlot.jsx
@@ -1,11 +1,28 @@
 /* eslint-disable no-restricted-globals */
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Container, MoneyInput, Unit } from 'components/orderArea/MoneySlot.style';
 
 export default function MoneySlot() {
+  const [payment, setPayMent] = useState('');
+
+  const isRightPayMent = inputValue => {
+    if (inputValue[0] === '0') return false;
+    if (isNaN(inputValue[inputValue.length - 1])) return false;
+    if (inputValue.length > 6) return false;
+    return true;
+  };
+
+  const handleChangeMoneyInput = ({ target }) => {
+    const inputValue = target.value;
+    const numPayMent = Number(inputValue.replace(/[,]/gim, ''));
+    if (isRightPayMent(inputValue)) setPayMent(numPayMent);
+  };
+
+  useEffect(() => {}, [payment]);
+
   return (
     <Container>
-      <MoneyInput type="text" />
+      <MoneyInput type="text" value={payment.toLocaleString('en')} onChange={handleChangeMoneyInput} />
       <Unit>원</Unit>
     </Container>
   );
