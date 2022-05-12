d613e1feb52628b3e219cdc0c0eb8b2196dee3ee
Muffin <jinlog9@gmail.com>
2022-05-13 07:56:39 +0900

    rename: 경로 변경

---
+++ src/Page/MyPage.tsx
@@ -0,0 +1,3 @@
+export default function MyPage(): JSX.Element {
+  return <div>Coming Soon...</div>;
+}
--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,5 @@
 import { createRoot } from 'react-dom/client';
-import App from './App';
+import App from '@/App';
 
 if (process.env.NODE_ENV === 'development') {
   const { worker } = require('./mocks/worker');
