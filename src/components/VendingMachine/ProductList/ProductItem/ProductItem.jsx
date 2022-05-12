698a663c46a39d0dadfeef501825afd1c111b75a
BB-choi <78826879+BB-choi@users.noreply.github.com>
2022-05-12 13:36:10 +0900

    refactor: 리뷰(1-1) 반영 리팩토링
    * chore: index.html 프로젝트에 알맞게 변경
    
    * refactor: NavBar내부 불필요한 컴포넌트 분리 삭제
    
    * refactor: ProductList ProductItem 코드 리팩토링

--- src/components/VendingMachine/ProductList/ProductItem/ProductItem.jsx
+++ src/components/VendingMachine/ProductList/ProductItem/ProductItem.jsx
@@ -2,17 +2,17 @@ import Button from "components/common/form/Button/Button";
 
 import { productButtonStyle, ProductLi } from "./ProductItem.styled";
 
-const ProductItem = ({ productsData }) => {
-  return productsData.map((product) => (
-    <ProductLi key={product.id}>
+const ProductItem = ({ productData }) => {
+  return (
+    <ProductLi>
       <Button
-        data={product}
+        data={productData}
         styles={productButtonStyle}
         className="product-button"
       />
-      <p className="product-price">{product.price}</p>
+      <p className="product-price">{productData.price}</p>
     </ProductLi>
-  ));
+  );
 };
 
 export default ProductItem;
