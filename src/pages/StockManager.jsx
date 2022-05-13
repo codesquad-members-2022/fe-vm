d9d39efb091e491c085c781a62a52e265519a632
happyGyu <happyboyto.dev@gmail.com>
2022-05-13 12:02:00 +0900

    Design: stock manager 초기 디자인. 기능 없음

--- src/pages/StockManager.jsx
+++ src/pages/StockManager.jsx
@@ -1,3 +1,32 @@
-const StockManager = () => {};
+import styled from "styled-components";
+import { useContext, useState } from "react";
+import { ProductsInfo } from "../contextProviders/ProductsInfoProvider";
+import ProductStockManager from "../components/StockManager/ProductStockManager";
+import SaveButton from "../components/StockManager/SaveButton";
+
+const StockManager = () => {
+  const { productsInfo } = useContext(ProductsInfo);
+  const [tempProductsInfo, setTempProductsInfo] = useState(productsInfo);
+
+  return (
+    <StockManagerWrapper>
+      {tempProductsInfo?.map((tempProductInfo) => (
+        <ProductStockManager
+          key={tempProductInfo.id}
+          tempProductInfo={tempProductInfo}
+          setTempProductsInfo={setTempProductsInfo}
+        />
+      ))}
+      <SaveButton tempProductsInfo={tempProductsInfo} />
+    </StockManagerWrapper>
+  );
+};
+
+const StockManagerWrapper = styled.div`
+  width: 400px;
+  height: 900px;
+  margin: 100px 0;
+  background-color: ${({ theme }) => theme.colors.grey4};
+`;
 
 export default StockManager;
