b55842ea7fd9f3a078fd8ea95d13b8a00ef7b808
ju-kkim <wndus8198@naver.com>
2022-05-13 15:28:34 +0900

    style: 불필요 주석 삭제

--- src/Components/Contents/PayBox/AddBtn.jsx
+++ src/Components/Contents/PayBox/AddBtn.jsx
@@ -8,7 +8,6 @@ export default function AddBtn({ payTotal, payMoney, input, message }) {
     input.current.value = '';
     if (isNaN(payMoney)) return;
     const updateTotal = payTotal.value + Number(payMoney);
-    // const MESSAGE = `${changeNumToLocalMoney(payMoney)}원을 투입했습니다.`;
     const MESSAGE = getMessage('투입', changeNumToLocalMoney(payMoney));
 
     payTotal.set(updateTotal);
