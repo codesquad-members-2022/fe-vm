d98992626c02885584641811212c237fdcb56bc2
seungchanHan <seungchanhan00@gmail.com>
2022-05-13 11:38:57 +0900

    feat : input에 한글만 입력

--- vending_machine/src/components/InputForm.jsx
+++ vending_machine/src/components/InputForm.jsx
@@ -2,6 +2,8 @@ import React, { useContext, useRef, useState } from 'react';
 import styled from 'styled-components';
 
 import { ProgressContext } from '../App';
+import { color, fontSize } from '../style/variables';
+import { changeKoreanLocalMoney } from '../utility/util';
 
 const InputForm = ({ totalMoney, setTotalMoney }) => {
   const [inputValue, setInputValue] = useState(0);
@@ -17,8 +19,10 @@ const InputForm = ({ totalMoney, setTotalMoney }) => {
     inputTag.current.value = '';
   };
 
-  const changeValue = ({ target: { value } }) => {
-    setInputValue(value);
+  const changeValue = () => {
+    const onlyNumber = inputTag.current.value.replace(/[^0-9]/g, '');
+    setInputValue(onlyNumber);
+    inputTag.current.value = changeKoreanLocalMoney(onlyNumber);
   };
 
   return (
@@ -39,13 +43,13 @@ const StyledForm = styled.form`
   width: 270px;
   height: 60px;
   margin-top: 20px;
-  border: 2px solid gray;
+  border: 2px solid ${color.grey};
 `;
 
 const StyledInput = styled.input`
   width: 230px;
   height: inherit;
-  font-size: 20px;
+  font-size: ${fontSize.xl};
   border: none;
   text-align: right;
   background-color: inherit;
@@ -56,7 +60,7 @@ const StyledInput = styled.input`
 `;
 
 const StyledSpan = styled.span`
-  font-size: 20px;
+  font-size: ${fontSize.xl};
 `;
 
 export default InputForm;
