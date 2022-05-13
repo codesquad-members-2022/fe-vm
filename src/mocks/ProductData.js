6ff594fdaf5fa312d39f5a75cd296086185fc55f
godhyeongman <aaaa4000@naver.com>
2022-05-13 16:43:22 +0900

    refactor: 피드백 반영 목데이터 id 적용 리팩토링

--- src/mocks/ProductData.js
+++ src/mocks/ProductData.js
@@ -1,3 +1,5 @@
+import {uuidv4} from '../Utils';
+
 export const PRODUCTS_DATA = [
   {title: '사이다', price: 900, quantity: 4},
   {title: '콜라', price: 1200, quantity: 4},
@@ -15,4 +17,4 @@ export const PRODUCTS_DATA = [
   {title: '미녀는 석류를 좋아해', price: 1200, quantity: 4},
   {title: '미녀는 석류를 좋아해', price: 1200, quantity: 4},
   {title: '미녀는 석류를 좋아해', price: 1200, quantity: 4},
-];
+].map(item => ({...item, id: uuidv4()}));
