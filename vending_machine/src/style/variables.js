3fb90d765e8e7def20766be7ef458299333d7c80
seungchanHan <seungchanhan00@gmail.com>
2022-05-13 11:35:59 +0900

    style : font-size && color 상수화

---
+++ vending_machine/src/style/variables.js
@@ -0,0 +1,17 @@
+const color = {
+  black: '#000',
+  grey: '#777',
+  red: '#f00',
+  blue: '#00f',
+  white: '#fff',
+};
+
+const fontSize = {
+  xl: '1.5rem',
+  l: '1.25rem',
+  m: '1rem',
+  s: '0.875rem',
+  xs: '0.75rem',
+};
+
+export { color, fontSize };
