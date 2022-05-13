0ff0017ab53b1b92bf52db9d1bae0b40a8e0f4c9
jindonyy <rsefa94@gmail.com>
2022-05-13 15:01:16 +0900

    Test: 지갑 목데이터 추가

---
+++ src/data/wallet.js
@@ -0,0 +1,39 @@
+const walletData = [
+  {
+    id: 1,
+    unit: 10,
+    quantity: 4
+  },
+  {
+    id: 2,
+    unit: 50,
+    quantity: 3
+  },
+  {
+    id: 3,
+    unit: 100,
+    quantity: 6
+  },
+  {
+    id: 4,
+    unit: 500,
+    quantity: 2
+  },
+  {
+    id: 5,
+    unit: 1000,
+    quantity: 3
+  },
+  {
+    id: 6,
+    unit: 5000,
+    quantity: 5
+  },
+  {
+    id: 7,
+    unit: 10000,
+    quantity: 6
+  }
+];
+
+export default walletData;
