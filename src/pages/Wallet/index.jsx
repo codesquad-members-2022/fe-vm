e4cd5149cb6c896df62295f790b500c6b6e0dac3
Millie <jay.lee.2280@gmail.com>
2022-05-13 12:53:55 +0900

    Design: wallet page (#8)

--- src/pages/Wallet/index.jsx
+++ src/pages/Wallet/index.jsx
@@ -1,7 +1,31 @@
 import React from 'react';
 
+import CashDisplay from '@/components/CashDisplay';
+import ItemBlock from '@/components/ItemBlock';
+import moneyData from '@/mock/money';
+import { Title, SubTitle } from '@/styles/common';
+
+import * as S from './Wallet.style';
+
 const Wallet = () => {
-  return <div>Wallet</div>;
+  return (
+    <S.WalletWrapper>
+      <Title>MY WALLET</Title>
+      <S.BalanceWrapper>
+        <SubTitle>BALANCE</SubTitle>
+        <CashDisplay isBalance={true} />
+      </S.BalanceWrapper>
+
+      <S.MoneyWrapper>
+        <SubTitle>ADD MONEY</SubTitle>
+        <S.MoneyItemWrapper>
+          {moneyData.map(money => (
+            <ItemBlock isMoney key={money.id} unit={money.unit} count={money.count} />
+          ))}
+        </S.MoneyItemWrapper>
+      </S.MoneyWrapper>
+    </S.WalletWrapper>
+  );
 };
 
 export default Wallet;
