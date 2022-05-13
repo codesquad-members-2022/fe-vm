c13bd58050f80a6af4f3967a2e749b07f3428e9c
suuzin <8oozin@gmail.com>
2022-05-13 14:27:20 +0900

    refactor : 누적금액명 수정

--- src/components/CurrentPrice/CurrentPrice.js
+++ src/components/CurrentPrice/CurrentPrice.js
@@ -1,6 +1,6 @@
 import "./CurrentPrice.css";
 const CurrentPrice = ({ accumulatedPrice }) => {
-  return <p className="current-price">현재금액 : {accumulatedPrice} 원</p>;
+  return <p className="current-price">누적 금액 : {accumulatedPrice} 원</p>;
 };
 
 export default CurrentPrice;
