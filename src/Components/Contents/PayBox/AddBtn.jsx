70346df883b1f29c6bb81c1d4d30d2336c435584
ju-kkim <wndus8198@naver.com>
2022-05-13 14:35:14 +0900

    feat: 투입버튼 클릭 이벤트 추가

--- src/Components/Contents/PayBox/AddBtn.jsx
+++ src/Components/Contents/PayBox/AddBtn.jsx
@@ -1,8 +1,27 @@
 import { FontSize } from '../../../Assets/Common.style';
+import { changeNumToLocalMoney, getMessage } from '../../../Utils/utils';
 import Btn from '../../Button';
 
-export default function AddBtn() {
+export default function AddBtn({ payTotal, payMoney, input, message }) {
+  const addBtnClickHandler = (e) => {
+    e.preventDefault();
+    input.current.value = '';
+    if (isNaN(payMoney)) return;
+    const updateTotal = payTotal.value + Number(payMoney);
+    // const MESSAGE = `${changeNumToLocalMoney(payMoney)}원을 투입했습니다.`;
+    const MESSAGE = getMessage('투입', changeNumToLocalMoney(payMoney));
+
+    payTotal.set(updateTotal);
+    message.set([...message.value, MESSAGE]);
+  };
+
   return (
-    <Btn type="button" title="ADD" textTag="span" fontSize={FontSize.LARGE} />
+    <Btn
+      type="submit"
+      title="ADD"
+      textTag="span"
+      fontSize={FontSize.LARGE}
+      eventHandler={addBtnClickHandler}
+    />
   );
 }
