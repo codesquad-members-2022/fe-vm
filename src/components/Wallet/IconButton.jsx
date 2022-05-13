c8289c43e135fff63ad136317a7bdba12607a36c
Sangbeom Heo <mansaout@gmail.com>
2022-05-13 18:01:33 +0900

    style: 코드 포맷 변경

--- src/components/Wallet/IconButton.jsx
+++ src/components/Wallet/IconButton.jsx
@@ -1,13 +1,13 @@
 import { ICON_TYPE } from 'constants';
 import { PlusIcon, MinusIcon } from 'components/icons';
 
-const IconButton = ({ type, handleClick }) => {
-  return <button onClick={handleClick}>{getIcon(type)}</button>;
-};
-
 const getIcon = type => {
   if (type === ICON_TYPE.PLUS) return <PlusIcon />;
   if (type === ICON_TYPE.MINUS) return <MinusIcon />;
+};
+
+const IconButton = ({ type, handleClick }) => {
+  return <button onClick={handleClick}>{getIcon(type)}</button>;
 };
 
 export default IconButton;
