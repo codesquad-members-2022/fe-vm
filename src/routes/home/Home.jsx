b6c9e96a2bca22f753f6ffdbd2a95a7505a1b699
nanjeong <whdwkxj7@naver.com>
2022-05-13 14:43:01 +0900

    fix: 뒤로가기 버튼 눌러도 해당하는 탭 버튼이 표시되도록 수정

--- src/routes/home/Home.jsx
+++ src/routes/home/Home.jsx
@@ -11,10 +11,23 @@ export const VendingMachineContext = React.createContext();
 
 function Home() {
     const { pathname } = useLocation();
-    const [currentTab, setCurrentTab] = useState(pathname.slice(1));
     const [record, setRecord] = useState([]);
     const [moneyInVendingMachine, setMoneyInVendingMachine] = useState({});
     const [wallet, setWallet] = useState(dataOfwallet);
+
+    const currentTab = (() => {
+        switch (pathname.slice(1)) {
+            case VM: {
+                return VM;
+            }
+            case WALLET: {
+                return WALLET;
+            }
+            default: {
+                return null;
+            }
+        }
+    })();
 
     const addRecord = (newRecord) => {
         setRecord([...record, newRecord]);
@@ -28,22 +41,15 @@ function Home() {
     const updateWallet = (newWallet) => {
         setWallet(newWallet);
     };
-    const changeTab = (newTab) => {
-        setCurrentTab(newTab);
-    };
 
     return (
         <HomeContainer>
             <TabNav>
                 <TabButton isCurrentTab={currentTab === VM}>
-                    <Link to="/vm" onClick={() => changeTab(VM)}>
-                        자판기
-                    </Link>
+                    <Link to="/vm">자판기</Link>
                 </TabButton>
                 <TabButton isCurrentTab={currentTab === WALLET}>
-                    <Link to="/wallet" onClick={() => changeTab(WALLET)}>
-                        지갑
-                    </Link>
+                    <Link to="/wallet">지갑</Link>
                 </TabButton>
             </TabNav>
             {pathname === "/" && <h1>버튼을 클릭하세요</h1>}
