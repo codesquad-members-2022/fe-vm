d95ac4b640fa5d4a27f1e198eeafb77aa8f375c4
seungchanHan <seungchanhan00@gmail.com>
2022-05-13 11:52:02 +0900

    feat : 투입금액 1000단위 콤마

--- vending_machine/src/components/Order.jsx
+++ vending_machine/src/components/Order.jsx
@@ -5,13 +5,17 @@ import { TotalMoneyContext } from '../App';
 import InputForm from './InputForm';
 import Message from './Message';
 import ReturnBtn from './ReturnBtn';
+import { changeKoreanLocalMoney } from '../utility/util';
+import { color, fontSize } from '../style/variables';
 
 const Order = () => {
-  const { totalMoney, setTotalMoney } = useContext(TotalMoneyContext);
+  const [totalMoney, setTotalMoney] = useContext(TotalMoneyContext);
 
   return (
     <>
-      <StyledTotal>투입금액 : {totalMoney}원</StyledTotal>
+      <StyledTotal>
+        투입금액 : {changeKoreanLocalMoney(totalMoney)}원
+      </StyledTotal>
       <InputForm totalMoney={totalMoney} setTotalMoney={setTotalMoney} />
       <ReturnBtn totalMoney={totalMoney} setTotalMoney={setTotalMoney} />
       <Message />
@@ -23,9 +27,9 @@ const StyledTotal = styled.span`
   display: inline-block;
   margin-top: 20px;
   width: 270px;
-  border: 2px solid gray;
+  border: 2px solid ${color.grey};
   padding: 20px 0;
-  font-size: 20px;
+  font-size: ${fontSize.xl};
 `;
 
 export default Order;
