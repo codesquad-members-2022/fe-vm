7d2d611c4749b1017280d5b838e930d5fb68abe8
dpfla8123 <dpfla8123@gmail.com>
2022-05-15 22:25:25 +0900

    feat: GlobalStyle, ThemeProvider 추가 - 관련 패키지 파일 추가 - App 컴포넌트 TabMenu -> Gnb로 관련 내용 네이밍 수정

---
+++ app/src/common/theme.js
@@ -0,0 +1,15 @@
+const theme = {
+  color: {
+    color_100: '#FFF',
+    color_200: '#EEE',
+    color_300: '#30475E',
+  },
+  fontSize: {
+    large: '20px',
+    medium: '16px',
+    small: '14px',
+  },
+  borderRadius: '10px',
+};
+
+export default theme;
