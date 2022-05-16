68e2d22b0061dc95064418a3ceab79f5e51528d4
Muffin <jinlog9@gmail.com>
2022-05-17 00:06:41 +0900

    refactor: WalletContext unit type union 변경

--- src/Context/WalletContext.tsx
+++ src/Context/WalletContext.tsx
@@ -2,7 +2,7 @@ import { createContext, Dispatch, useReducer, useContext } from 'react';
 
 interface IWallet {
   uuid: number;
-  unit: number;
+  unit: 10 | 50 | 100 | 500 | 1000 | 5000 | 10000;
   count: number;
 }
 
