1d1365b34d1013a3b5b57ee788d44016408bf4dc
Serin-Kim <serin9864@gmail.com>
2022-05-16 17:41:58 +0900

    fix: inputMoney state가 하나씩 밀리는 것 해결
    inputMoney 가 최신화 되었다고 가정하고 코드를 진행했기 때문에 발생한 문제였음

--- src/components/Information/Information.jsx
+++ src/components/Information/Information.jsx
@@ -3,19 +3,18 @@ import { StyledInformation, InputPrice, ChangeButton, ActionLog } from "./Inform
 import { MoneyContext, LogContext } from "../../App.js";
 
 function Information() {
-  const { inputMoney, setInputMoney } = useContext(MoneyContext);
+  const { setInputMoney } = useContext(MoneyContext);
   const { logs, setLogs } = useContext(LogContext);
 
   const getMoney = (e) => {
     if (e.key === "Enter") {
       setInputMoney(+e.target.value);
-      addInsertLog();
+      addInsertLog(e.target.value);
     }
   };
 
-  const addInsertLog = () => {
-    // TODO: inputMoney state 가 하나씩 밀리는 것 해결
-    const newLog = { idx: logs.length + 1, type: "insert", data: inputMoney + "원이 투입됨" };
+  const addInsertLog = (money) => {
+    const newLog = { idx: logs.length + 1, type: "insert", data: money + "원이 투입됨" };
     setLogs([...logs, newLog]);
   };
 
