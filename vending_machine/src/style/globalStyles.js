a392944b336d3ed1023a794972daca2bce9606fe
seungchanHan <seungchanhan00@gmail.com>
2022-05-17 02:56:44 +0900

    feat : 폰트 변경

--- vending_machine/src/style/globalStyles.js
+++ vending_machine/src/style/globalStyles.js
@@ -2,11 +2,7 @@ import { createGlobalStyle } from 'styled-components';
 import normalize from './normalize.css';
 
 const GlobalStyle = createGlobalStyle`
-@import url('https://fonts.googleapis.com/css2?family=Nanum+Pen+Script&display=swap');
     ${normalize}; 
-    * {
-    font-family: 'Nanum Pen Script', cursive;
-    }
 
     button{
       background: none;
