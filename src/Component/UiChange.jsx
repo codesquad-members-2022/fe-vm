64d27c99717c04bdce5a72d75710e59c73afbe88
godhyeongman <aaaa4000@naver.com>
2022-05-17 01:26:02 +0900

    refactor: 컨텍스트 로직 변경에 현재 UI 상태 props 전달 로직 구현

--- src/Component/UiChange.jsx
+++ src/Component/UiChange.jsx
@@ -3,14 +3,28 @@ import {Link} from 'react-router-dom';
 
 import styled from 'styled-components';
 
-export const UiChangeBtn = () => {
+const DEFAULT_PAGE = 'vendingMachine';
+const USER_WALLET = 'wallet';
+
+export const UiChangeBtn = ({handleChangeBtn, currentPage}) => {
   return (
     <ButtonWrapper>
-      <Link to="/vendingMachine">
+      <Link
+        to="/vendingMachine"
+        onClick={e => {
+          handleChangeBtn(e, currentPage, DEFAULT_PAGE);
+        }}
+      >
         <ChangeBtn>자판기</ChangeBtn>
       </Link>
       <Link to="/wallet">
-        <ChangeBtn>지갑</ChangeBtn>
+        <ChangeBtn
+          onClick={e => {
+            handleChangeBtn(e, currentPage, USER_WALLET);
+          }}
+        >
+          지갑
+        </ChangeBtn>
       </Link>
     </ButtonWrapper>
   );
@@ -22,10 +36,6 @@ const ChangeBtn = styled.button`
   border: 1px solid black;
   background-color: white;
   cursor: pointer;
-
-  // :first-child {
-  //   border-right: none;
-  // }
 
   :hover {
     background-color: rgba(0, 0, 0, 0.5);
