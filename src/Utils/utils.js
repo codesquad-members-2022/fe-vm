71bf22d980b4b6a783d592d38bc16ae9d9057026
ju-kkim <wndus8198@naver.com>
2022-05-16 23:51:25 +0900

    feat: 숫자 제외 공백처리 함수 utils로 빼기

--- src/Utils/utils.js
+++ src/Utils/utils.js
@@ -1,3 +1,7 @@
+export function replaceNotNumToSpace(str) {
+  return str.replace(/[^0-9]|,/g, ''); // 숫자 아닐때 공백처리
+}
+
 export function changeNumToLocalMoney(num) {
   return Number(num).toLocaleString('ko-KR');
 }
