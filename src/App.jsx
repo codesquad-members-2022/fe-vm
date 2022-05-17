c1191797b62c678c51ca459732d595c7a4dd8048
decay95 <decay95@naver.com>
2022-05-17 12:32:18 +0900

    [feat]라우트 기능 설정

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,23 @@
 import React from 'react';
+import { Route, Routes } from 'react-router-dom';
+import GlobalStyle from 'Assets/Style/Global';
+import GNB from 'components/Header/GNB';
+import VM from 'pages/VM';
+import Wallet from 'pages/Wallet';
+import Store from 'pages/Store';
 
-const App = () => {};
+const App = () => {
+  return (
+    <>
+      <GlobalStyle />
+      <GNB />
+      <Routes>
+        <Route path="/" element={<VM />} />
+        <Route path="/vm" element={<VM />} />
+        <Route path="/wallet" element={<Wallet />} />
+        <Route path="/store" element={<Store />} />
+      </Routes>
+    </>
+  );
+};
 export default App;
