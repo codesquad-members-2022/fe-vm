960205dabfc7edc911219864a4b4ad55990dc2f3
mina-gwak <mina.gwak@kakao.com>
2022-05-17 20:51:11 +0900

    feat: Create money data (#7)

---
+++ src/data/money.js
@@ -0,0 +1,39 @@
+const money = [
+  {
+    id: 1,
+    unit: 10,
+    quantity: 5,
+  },
+  {
+    id: 2,
+    unit: 50,
+    quantity: 5,
+  },
+  {
+    id: 3,
+    unit: 100,
+    quantity: 5,
+  },
+  {
+    id: 4,
+    unit: 500,
+    quantity: 5,
+  },
+  {
+    id: 5,
+    unit: 1000,
+    quantity: 5,
+  },
+  {
+    id: 6,
+    unit: 5000,
+    quantity: 5,
+  },
+  {
+    id: 7,
+    unit: 10000,
+    quantity: 5,
+  },
+];
+
+export default money;
