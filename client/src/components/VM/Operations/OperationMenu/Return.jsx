929422b8ab3dd8b8e41a2f16e9f3fa20802ed901
ver <luzverde0314@gmail.com>
2022-05-17 14:21:56 +0900

    Feat: 반환알고리즘 구현

--- client/src/components/VM/Operations/OperationMenu/Return.jsx
+++ client/src/components/VM/Operations/OperationMenu/Return.jsx
@@ -2,17 +2,23 @@ import React, { useContext } from "react";
 import styled from "styled-components";
 import { InputContext } from "store/InputStore";
 import { MessageContext } from "store/MessageStore";
+import { WalletContext } from "store/WalletStore";
 
 export default function Return() {
   const inputContext = useContext(InputContext);
   const { input, setInput } = inputContext;
   const messageContext = useContext(MessageContext);
   const { setMessage } = messageContext;
+  const walletContext = useContext(WalletContext);
+  const { wallet, setWallet } = walletContext;
 
   return (
     <StyledReturn
       onClick={() => {
         if (input <= 0) return;
+        //원본의 불변성을 지켜야할지 렌더링을 줄여야할지
+        //pipe 만들기??
+        setWallet(getAddedWallet(wallet, getAddedMoney(input)));
         setInput(0);
         setMessage((prev) => [...prev, `잔돈 ${input}원이 반환되었습니다 \n`]);
       }}
@@ -34,3 +40,28 @@ const StyledReturn = styled.button`
   font-weight: bold;
   background-color: lightgray;
 `;
+
+function getAddedMoney(inputMoney) {
+  let total = inputMoney;
+  const units = [10000, 5000, 1000, 500, 100, 50, 10];
+  const addedMoney = [];
+
+  units.forEach((unit) => {
+    const quotient = (total / unit) >> 0;
+    const remainder = total % unit;
+    if (quotient >= 1) {
+      addedMoney.push([unit, quotient]);
+      total = remainder;
+    }
+  });
+
+  return addedMoney;
+}
+
+function getAddedWallet(wallet, moneyArr) {
+  const newWallet = { ...wallet };
+  moneyArr.forEach(([unit, count]) => {
+    newWallet[unit] += count;
+  });
+  return newWallet;
+}
