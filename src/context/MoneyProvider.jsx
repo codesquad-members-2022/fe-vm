7d3b5ebbc9f855c3429e80067809a7434d92beae
healtheloper <58503584+healtheloper@users.noreply.github.com>
2022-05-17 17:26:23 +0900

    feat: vm item click -> 거스름돈 로직구현

--- src/context/MoneyProvider.jsx
+++ src/context/MoneyProvider.jsx
@@ -55,6 +55,100 @@ const calcWalletMoney = ({
   };
 };
 
+const getCoinsByAmount = (amount) => {
+  let changedAmount = amount;
+  return moneyAmount.reduce((acc, nextAmount) => {
+    const amountCount = Math.floor(changedAmount / nextAmount);
+    acc[nextAmount] = amountCount > 0 ? amountCount : 0;
+    if (amountCount > 0 && changedAmount > 0) {
+      changedAmount -= amountCount * nextAmount;
+    }
+    return acc;
+  }, {});
+};
+
+const getSortedInsertedMoney = (insertedMoney) =>
+  Object.entries(insertedMoney)
+    .sort((a, b) => b[0] - a[0])
+    .map(([key, val]) => [+key, +val]);
+
+const getRestCoins = (newInserted) => {
+  const firstMinusCountIdx = newInserted.findIndex(([, count]) => count < 0);
+  const restCoins = newInserted.slice(firstMinusCountIdx, newInserted.length);
+  const sumRestCoins = restCoins.reduce((sum, next) => {
+    const [amount, count] = next;
+    return sum + amount * count;
+  }, 0);
+  const isSumOverZero = sumRestCoins >= 0;
+  if (!isSumOverZero) {
+    let tempSumRestCoins = sumRestCoins;
+    for (let i = 1; i <= firstMinusCountIdx; i++) {
+      const biggerStartIdx = firstMinusCountIdx - i;
+      const [amount, count] = newInserted[biggerStartIdx];
+      for (let j = 0; j < count; j++) {
+        tempSumRestCoins += amount;
+        if (tempSumRestCoins >= 0) {
+          const slicedInserted = newInserted.slice(
+            biggerStartIdx + 1,
+            newInserted.length
+          );
+          return [[amount, count - j], ...slicedInserted];
+        }
+      }
+    }
+  }
+  return restCoins;
+};
+
+const getDiffInsertedMoney = (originInserted, minusInserted) => {
+  const newMinusInserted = getSortedInsertedMoney(minusInserted);
+  const newOriginInserted = getSortedInsertedMoney(originInserted);
+  const newInserted = newOriginInserted.map(([amount, count], idx) => {
+    const [, minusCount] = newMinusInserted[idx];
+    return [amount, count - minusCount];
+  });
+  const restCoins = getRestCoins(newInserted);
+  let tempSumRest = 0;
+  const minusToZeroCoins = restCoins.map((coin) => {
+    const [amount, count] = coin;
+    const newCount = count < 0 ? 0 : count;
+    if (count < 0) {
+      tempSumRest += amount * count;
+    }
+    return [amount, newCount];
+  });
+  const pullingCoins = minusToZeroCoins.map((coin) => {
+    const [amount, count] = coin;
+    let newCount = count;
+    while (newCount > 0 && tempSumRest < 0) {
+      tempSumRest += amount;
+      newCount -= 1;
+    }
+    return [amount, newCount];
+  });
+
+  const newReturnCoins = getCoinsByAmount(tempSumRest);
+
+  const newRestCoins = pullingCoins.map((coin) => {
+    const [amount, count] = coin;
+    const returnCoinCount = newReturnCoins[amount];
+    return [amount, count + returnCoinCount];
+  });
+
+  const newInsertedObj = Object.fromEntries(
+    newRestCoins.sort((a, b) => a[0] - b[0])
+  );
+
+  const newRestCoinsObj = Object.fromEntries(
+    newInserted.sort((a, b) => a[0] - b[0])
+  );
+
+  return {
+    ...newRestCoinsObj,
+    ...newInsertedObj,
+  };
+};
+
 const initMoneyState = {
   inputMoney: 0,
   insertedMoney: initInsertedMoney,
@@ -136,7 +230,16 @@ export const MoneyProvider = (props) => {
 
   const buyVMItem = (itemAmount, name) => {
     const newInputMoney = inputMoney - itemAmount;
-    dispatchMoney({ type: 'CHANGE_INPUT_MONEY', inputMoney: newInputMoney });
+    const itemRequiredCoins = getCoinsByAmount(itemAmount);
+    const newInsertedMoney = getDiffInsertedMoney(
+      insertedMoney,
+      itemRequiredCoins
+    );
+    const newState = {
+      inputMoney: newInputMoney,
+      insertedMoney: newInsertedMoney,
+    };
+    dispatchMoney({ type: 'CHANGE_ALL', newState });
     insertLog({
       type: 'select',
       data: name,
