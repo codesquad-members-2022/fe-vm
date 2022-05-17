48ceff4bd51131ccd0bc51f72c6d207f505f2776
Muffin <jinlog9@gmail.com>
2022-05-17 11:53:42 +0900

    feat: InputMoney 컴포넌트 기능 구현

--- src/Components/Machine/InputMoney.tsx
+++ src/Components/Machine/InputMoney.tsx
@@ -1,7 +1,7 @@
 import styled from 'styled-components';
 import { useState } from 'react';
 
-import { useWalletState } from '@/Context/WalletContext';
+import { useWalletState, useWalletDispatch } from '@/Context/WalletContext';
 import { useMessageDispatch } from '@/Context/MessageContext';
 import { usePriceState, usePriceDispatch } from '@/Context/PriceContext';
 
@@ -21,6 +21,7 @@ export default function InputMoney(): JSX.Element {
   const [price, setPrice] = useState<number | undefined>(undefined);
   const priceState = usePriceState();
   const walletState = useWalletState();
+  const walletDispatch = useWalletDispatch();
   const priceDispatch = usePriceDispatch();
   const messageDispatch = useMessageDispatch();
   let sumPrice = 0;
@@ -28,6 +29,14 @@ export default function InputMoney(): JSX.Element {
   walletState.forEach(wallet => {
     sumPrice += wallet.unit * wallet.count;
   });
+
+  const isCheckUnit = (unit: number) => {
+    for (const wallet of walletState) {
+      if (wallet.unit === unit) {
+        return wallet.count > 0 ? true : false;
+      }
+    }
+  };
 
   const handlePriceChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     let value = parseInt(e.target.value);
@@ -44,20 +53,43 @@ export default function InputMoney(): JSX.Element {
     if (typeof price === undefined) return 0;
     else if (price < 10) return 10;
 
-    return Math.floor(price / 10) * 10;
+    const units = [10, 50, 100, 500, 1000, 5000, 10000];
+    let differenceValue = units[units.length - 1];
+    let unitIdx = 0;
+
+    units.forEach((unit, idx) => {
+      if (differenceValue >= Math.abs(unit - price)) {
+        unitIdx = idx;
+        differenceValue = Math.abs(unit - price);
+      }
+    });
+
+    return units[unitIdx];
   };
 
   const handleKeyPressEvent = (e: React.KeyboardEvent<HTMLInputElement>) => {
     if (e.code === 'Enter') {
       if (isNaN(price) || price < 0) return;
+      else {
+        const unit = parsingUnit(price);
+        if (unit > sumPrice) {
+          alert('투입된 금액이 초과되었습니다.');
+          return;
+        }
 
-      const unit = parsingUnit(price);
-      const message = `${unit}원이 투입됐음`;
-      priceDispatch({ type: 'ADD_PRICE', price: unit });
-      messageDispatch({ type: 'INSERT_MESSAGE', unit, message });
-      setPrice(undefined);
+        if (!isCheckUnit(unit)) {
+          alert(`${unit}원이 지갑에 존재하지 않습니다.`);
+          return;
+        }
 
-      // 지갑에서 금액만큼 차감
+        const message = `${unit}원이 투입됐음`;
+
+        priceDispatch({ type: 'ADD_PRICE', price: unit });
+        messageDispatch({ type: 'INSERT_MESSAGE', unit, message });
+        walletDispatch({ type: 'DECREASE_WALLET_UNIT', unit, count: 1 });
+
+        setPrice(undefined);
+      }
     }
   };
 
