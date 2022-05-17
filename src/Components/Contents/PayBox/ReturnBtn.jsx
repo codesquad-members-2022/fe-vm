224e6a11b4e82803fd2d0aa9806633123c3cafa1
ju-kkim <wndus8198@naver.com>
2022-05-17 11:28:22 +0900

    feat: 반환버튼 클릭이벤트(payTotal 초기화)

--- src/Components/Contents/PayBox/ReturnBtn.jsx
+++ src/Components/Contents/PayBox/ReturnBtn.jsx
@@ -1,13 +1,23 @@
+import { useContext } from 'react';
+import { contentsContext } from '../../MainContents';
 import { FontSize } from '../../../Assets/Common.style';
 import Btn from '../../Btn';
 
+const resetPayNum = 0;
+
 export default function ReturnBtn() {
+  const { setPayTotal } = useContext(contentsContext);
+  const returnBtnClickHandler = () => {
+    setPayTotal(resetPayNum);
+  };
+
   return (
     <Btn
       type="button"
       title="RETURN"
       textTag="span"
       fontSize={FontSize.LARGE}
+      eventHandler={returnBtnClickHandler}
     />
   );
 }
