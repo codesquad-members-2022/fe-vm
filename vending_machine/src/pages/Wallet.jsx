27cd176e9b8c0aa709ae776ccab8937204d1c2fa
seungchanHan <seungchanhan00@gmail.com>
2022-05-17 20:40:38 +0900

    feat : 동전 생성

--- vending_machine/src/pages/Wallet.jsx
+++ vending_machine/src/pages/Wallet.jsx
@@ -1,7 +1,46 @@
-import React from 'react';
+import React, { useEffect, useState, useContext } from 'react';
+import styled from 'styled-components';
+
+import { TotalMoneyContext } from '../App';
+import SumMoney from '../components/SumMoney';
+import TotalMoney from '../components/TotalMoney';
+import { flexBetween } from '../style/mixins';
+import { getData } from '../utility/util';
+import WalletItem from '../components/WalletItem';
 
 const Wallet = () => {
-  return <div>여기는 지갑 입니다</div>;
+  const [coinData, setCoinData] = useState([]);
+  const [totalMoney, setTotalMoney] = useContext(TotalMoneyContext);
+
+  useEffect(() => {
+    const coinUrl = `${process.env.PUBLIC_URL}/data/coin.json`;
+
+    getData(coinUrl, setCoinData);
+  }, []);
+
+  return (
+    <WalletContainer>
+      {coinData.map(({ id, unit, quantity }) => (
+        <WalletBox key={id}>
+          <WalletItem unit={unit} quantity={quantity} />
+        </WalletBox>
+      ))}
+      <TotalMoney totalMoney={totalMoney} />
+      <SumMoney coinData={coinData} />
+    </WalletContainer>
+  );
 };
+
+const WalletContainer = styled.ul`
+  margin: 0 auto;
+  width: 400px;
+  height: 1000px;
+  border: 2px solid black;
+`;
+
+const WalletBox = styled.li`
+  ${flexBetween}
+  font-size: 30px;
+`;
 
 export default Wallet;
