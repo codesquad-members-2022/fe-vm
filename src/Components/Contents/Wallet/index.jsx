68815c4b3dc0edf5bb8b88d5e2149c9c23d597b0
ju-kkim <wndus8198@naver.com>
2022-05-18 00:40:49 +0900

    feat: Wallet 페이지 추가

--- src/Components/Contents/Wallet/index.jsx
+++ src/Components/Contents/Wallet/index.jsx
@@ -1,5 +1,27 @@
+import styled from 'styled-components';
+import MessageBox from '../MessageBox';
 import { ContentBox } from '../style';
+import MoneyDtails from './MoneyDtails';
+import MoneyTotal from './MoneyTotal';
+import PayInfo from './PayInfo';
 
 export default function Wallet() {
-  return <ContentBox>지갑영역</ContentBox>;
+  return (
+    <ContentBox>
+      <Inner>
+        <MoneyTotal />
+        <MoneyDtails />
+        <PayInfo />
+        <MessageBox page="wallet" />
+      </Inner>
+    </ContentBox>
+  );
 }
+
+const Inner = styled.div`
+  padding: 0 20px;
+
+  h3 {
+    text-align: left;
+  }
+`;
--- src/Components/MainContents.jsx
+++ src/Components/MainContents.jsx
@@ -3,21 +3,38 @@ import { createContext } from 'react';
 import { Route, Routes } from 'react-router';
 import VendingMachine from './Contents/VendingMachine/index';
 import Wallet from './Contents/Wallet/index';
+import moneyData from './Contents/moneyData.json';
 
-export const contentsContext = createContext(0);
+export const contentsContext = createContext();
+export const myMoneyContext = createContext();
 
 export default function MainContents() {
+  const initMyMoney = moneyData.reduce(
+    (prev, next) => prev + next.value * next.count,
+    0,
+  );
   const [payTotal, setPayTotal] = useState(0);
   const [printMessages, setPrintMessages] = useState([]);
+  const [myMoneyDetails, setMyMoneyDetails] = useState(moneyData);
+  const [myMoneyTotal, setMyMoneyTotal] = useState(initMyMoney);
 
   return (
     <contentsContext.Provider
       value={{ payTotal, setPayTotal, printMessages, setPrintMessages }}
     >
-      <Routes path="/">
-        <Route index element={<VendingMachine />} />
-        <Route path="/wallet" element={<Wallet />} />
-      </Routes>
+      <myMoneyContext.Provider
+        value={{
+          myMoneyDetails,
+          setMyMoneyDetails,
+          myMoneyTotal,
+          setMyMoneyTotal,
+        }}
+      >
+        <Routes path="/">
+          <Route index element={<VendingMachine />} />
+          <Route path="/wallet" element={<Wallet />} />
+        </Routes>
+      </myMoneyContext.Provider>
     </contentsContext.Provider>
   );
 }
