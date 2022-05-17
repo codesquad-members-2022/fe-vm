49491c8f1fe1d62c2805a68b47ae56bf7aec8a6c
bangdler <zbthz90@gmail.com>
2022-05-17 17:53:38 +0900

    feat: context 추가

--- vending-machine/src/components/vendingMachine/vendingMachine.js
+++ vending-machine/src/components/vendingMachine/vendingMachine.js
@@ -4,13 +4,17 @@ import { StyledVMContainer } from './vendingMachine.styled';
 import { ShowWindow } from './showWindow/showWindow';
 import { UserWindow } from './userWindow/userWindow';
 
-export const InputMoneyContext = createContext();
-export const LogContext = createContext();
+export const InputMoneyContext = createContext(0);
+export const LogContext = createContext([]);
+export const PaybackContext = createContext(false);
+export const ProgressContext = createContext(false);
 
 export function VendingMachine() {
   const [itemList, setItemList] = useState([]);
   const [inputMoney, setInputMoney] = useState(0);
   const [logList, setLogList] = useState([]);
+  const [inProgress, setInProgress] = useState(false);
+  const [paybackState, setPaybackState] = useState(false);
 
   useEffect(() => {
     setItemList(itemData);
@@ -19,10 +23,14 @@ export function VendingMachine() {
   return (
     <LogContext.Provider value={{ logList, setLogList }}>
       <InputMoneyContext.Provider value={{ inputMoney, setInputMoney }}>
-        <StyledVMContainer>
-          <ShowWindow itemList={itemList} />
-          <UserWindow />
-        </StyledVMContainer>
+        <PaybackContext.Provider value={{ paybackState, setPaybackState }}>
+          <ProgressContext.Provider value={{ inProgress, setInProgress }}>
+            <StyledVMContainer>
+              <ShowWindow itemList={itemList} />
+              <UserWindow />
+            </StyledVMContainer>
+          </ProgressContext.Provider>
+        </PaybackContext.Provider>
       </InputMoneyContext.Provider>
     </LogContext.Provider>
   );
