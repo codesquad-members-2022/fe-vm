8bb66124f0a4c67618f5b5fd79a8a357ad2f730d
mina-gwak <mina.gwak@kakao.com>
2022-05-18 04:42:29 +0900

    feat: Add input box component (#11)
    - Add click & keydown handler

---
+++ src/components/atoms/InputBox/InputBox.style.js
@@ -0,0 +1,22 @@
+import styled from 'styled-components';
+
+import { COLOR } from '@constants';
+
+const Container = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 250px;
+  height: 40px;
+  background-color: ${COLOR.OFF_WHITE};
+  border-radius: 7px;
+  cursor: text;
+`;
+
+const Input = styled.p`
+  &:focus {
+    outline: none;
+  }
+`;
+
+export { Container, Input };
