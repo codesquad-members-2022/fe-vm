dfd055054e46624d663c55656b6723220bd4b989
Serin-Kim <serin9864@gmail.com>
2022-05-17 22:46:45 +0900

    feat: 입력 금액 자동 보정, 동전 반환 기능 구현

--- src/components/Information/Information.jsx
+++ src/components/Information/Information.jsx
@@ -6,16 +6,33 @@ import {
   ChangeButton,
   ActionLog,
 } from "./Information.styled";
-import { MoneyContext, LogContext } from "../../App.js";
+import { MoneyContext, LogContext, WalletContext } from "../../App.js";
+import { getTotalAmount } from "utils";
 
 function Information() {
   const { inputMoney, setInputMoney } = useContext(MoneyContext);
   const { logs, setLogs } = useContext(LogContext);
+  const { walletMoney } = useContext(WalletContext);
+
+  const convertInputMoney = (money) => {
+    let surplus = money > getTotalAmount(walletMoney) ? getTotalAmount(walletMoney) : money;
+
+    for (let i = walletMoney.length - 1; i >= 0; i--) {
+      if (surplus >= walletMoney[i].price && walletMoney[i].quantity > 0) {
+        const quantity = Math.min(Math.floor(surplus / walletMoney[i].price), walletMoney[i].quantity);
+        surplus = surplus - quantity * walletMoney[i].price;
+        walletMoney[i] = { ...walletMoney[i], quantity: walletMoney[i].quantity - quantity };
+      }
+    }
+
+    return money - surplus;
+  };
 
   const getMoney = (e) => {
     if (e.key === "Enter") {
-      setInputMoney(+e.target.value + inputMoney);
-      addInsertLog(e.target.value);
+      const convertedInputMoney = convertInputMoney(+e.target.value);
+      setInputMoney(convertedInputMoney + inputMoney); // 총 금액
+      addInsertLog(convertedInputMoney); // 방금 넣은 금액
     }
   };
 
@@ -24,11 +41,30 @@ function Information() {
     setLogs([...logs, newLog]);
   };
 
+  const addReturnLog = (money) => {
+    const newLog = { idx: logs.length + 1, type: "return", data: money + "원이 반환됨" };
+    setLogs([...logs, newLog]);
+  };
+
+  const returnChange = () => {
+    let change = inputMoney;
+    setInputMoney(inputMoney - change);
+    addReturnLog(change);
+
+    for (let i = walletMoney.length - 1; i >= 0; i--) {
+      if (change >= walletMoney[i].price) {
+        const returnedCoinQuantity = Math.floor(change / walletMoney[i].price);
+        change = change % walletMoney[i].price;
+        walletMoney[i] = { ...walletMoney[i], quantity: walletMoney[i].quantity + returnedCoinQuantity }; // set함수 안 쓰고 이렇게도 변경해도 되나
+      }
+    }
+  };
+
   return (
     <StyledInformation>
       <InputPrice onKeyPress={getMoney} placeholder="금액을 입력하세요"></InputPrice>
-      <TotalInputPrice>총 금액: {inputMoney}</TotalInputPrice>
-      <ChangeButton>
+      <TotalInputPrice>투입 금액: {inputMoney}</TotalInputPrice>
+      <ChangeButton onClick={returnChange}>
         <p>반환</p>
       </ChangeButton>
       <ActionLog>
