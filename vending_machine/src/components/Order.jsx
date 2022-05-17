6ed99d5a99876abd2b28d7a85e8e861ed55c4ff8
seungchanHan <seungchanhan00@gmail.com>
2022-05-17 20:44:44 +0900

    feat : totalMoney 컴포넌트 생석

--- vending_machine/src/components/Order.jsx
+++ vending_machine/src/components/Order.jsx
@@ -1,35 +1,22 @@
 import React, { useContext } from 'react';
-import styled from 'styled-components';
 
 import { TotalMoneyContext } from '../App';
 import InputForm from './InputForm';
 import Message from './Message';
 import ReturnBtn from './ReturnBtn';
-import { changeKoreanLocalMoney } from '../utility/util';
-import { color, fontSize } from '../style/variables';
+import TotalMoney from './TotalMoney';
 
 const Order = () => {
   const [totalMoney, setTotalMoney] = useContext(TotalMoneyContext);
 
   return (
     <>
-      <StyledTotal>
-        투입금액 : {changeKoreanLocalMoney(totalMoney)}원
-      </StyledTotal>
+      <TotalMoney totalMoney={totalMoney} />
       <InputForm totalMoney={totalMoney} setTotalMoney={setTotalMoney} />
       <ReturnBtn totalMoney={totalMoney} setTotalMoney={setTotalMoney} />
       <Message />
     </>
   );
 };
-
-const StyledTotal = styled.span`
-  display: inline-block;
-  margin-top: 20px;
-  width: 270px;
-  border: 2px solid ${color.gray};
-  padding: 20px 0;
-  font-size: ${fontSize.xl};
-`;
 
 export default Order;
