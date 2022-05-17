4d9521734177c1f52c87adc0c5157a132b65bc6b
jthw1005 <skl0105@naver.com>
2022-05-18 03:56:03 +0900

    Refactor: Modify routing components

--- src/App.js
+++ src/App.js
@@ -1,26 +1,23 @@
 import { Reset } from "styled-reset";
 import { ThemeProvider } from "styled-components";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Layout from "page/Layout";
+import { Routes, Route } from "react-router-dom";
 import VendingMachine from "page/VendingMachine";
 import Wallet from "page/Wallet";
 import NotFound from "page/NotFound";
 import theme from "theme";
+import NavBtn from "component/NavBtn";
 
 function App() {
     return (
         <>
+            <Reset />
             <ThemeProvider theme={theme}>
-                <Reset />
-                <BrowserRouter>
-                    <Routes>
-                        <Route path="/" element={<Layout />}>
-                            <Route index element={<VendingMachine />} />
-                            <Route path="/wallet" element={<Wallet />} />
-                            <Route path="*" element={<NotFound />} />
-                        </Route>
-                    </Routes>
-                </BrowserRouter>
+                <NavBtn />
+                <Routes>
+                    <Route path="/" element={<VendingMachine />} />
+                    <Route path="/wallet" element={<Wallet />} />
+                    <Route path="*" element={<NotFound />} />
+                </Routes>
             </ThemeProvider>
         </>
     );
