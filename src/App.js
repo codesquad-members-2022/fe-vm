ba4f7f01d5717ef55eb43bb8d0ab706c2ddd97a8
Serin-Kim <serin9864@gmail.com>
2022-05-17 16:10:59 +0900

    feat: ActionLog 에 Wallet 동전 삽입 추가

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { Layout } from "components";
 import { VendingMachineContainer, Wallet, NotFound } from "pages";
 import { createGlobalStyle } from "styled-components";
 import reset from "styled-reset";
+import { walletItem } from "data";
 
 const GlobalStyles = createGlobalStyle`
   ${reset}
@@ -20,24 +21,29 @@ const GlobalStyles = createGlobalStyle`
 
 export const MoneyContext = React.createContext();
 export const LogContext = React.createContext();
+export const WalletContext = React.createContext();
 
 function App() {
   const [inputMoney, setInputMoney] = useState(0);
   const [logs, setLogs] = useState([{ idx: 1, type: "init", data: "~ this is vending machine ~" }]);
+  const [walletMoney, setWalletMoney] = useState(walletItem);
 
   return (
+    // TODO: Circular Dependency 문제 해결 -> context 분리 ...?
     <LogContext.Provider value={{ logs, setLogs }}>
-      <MoneyContext.Provider value={{ inputMoney, setInputMoney }}>
-        <GlobalStyles />
-        <Routes>
-          <Route element={<Layout />}>
-            <Route index element={<VendingMachineContainer />} />
-            <Route path="/vendingmachine" element={<VendingMachineContainer />} />
-            <Route path="/wallet" element={<Wallet />} />
-          </Route>
-          <Route path="*" element={<NotFound />} />
-        </Routes>
-      </MoneyContext.Provider>
+      <WalletContext.Provider value={{ walletMoney, setWalletMoney }}>
+        <MoneyContext.Provider value={{ inputMoney, setInputMoney }}>
+          <GlobalStyles />
+          <Routes>
+            <Route element={<Layout />}>
+              <Route index element={<VendingMachineContainer />} />
+              <Route path="/vendingmachine" element={<VendingMachineContainer />} />
+              <Route path="/wallet" element={<Wallet />} />
+            </Route>
+            <Route path="*" element={<NotFound />} />
+          </Routes>
+        </MoneyContext.Provider>
+      </WalletContext.Provider>
     </LogContext.Provider>
   );
 }
--- src/pages/Wallet/Wallet.jsx
+++ src/pages/Wallet/Wallet.jsx
@@ -1,10 +1,11 @@
-import React, { useState } from "react";
-import { walletItem } from "data";
+import React, { useContext } from "react";
 import { StyledWallet, WalletDetail, TotalAmout, CoinPrice, CoinCount } from "./Wallet.styled";
 import { addComma } from "utils";
+import { LogContext, WalletContext } from "../../App.js";
 
 function Wallet() {
-  const [walletMoney, setWalletMoney] = useState(walletItem);
+  const { logs, setLogs } = useContext(LogContext);
+  const { walletMoney, setWalletMoney } = useContext(WalletContext);
 
   const getTotalAmout = () => {
     const totalAmout = walletMoney.reduce(function (acc, cur) {
@@ -14,15 +15,22 @@ function Wallet() {
     return totalAmout;
   };
 
+  // TODO: 로그 추가하는 함수가 Information 컴포넌트 함수와 동일함, 개선하기
+  const addInsertLog = (money) => {
+    const newLog = { idx: logs.length + 1, type: "insert", data: money + "원이 투입됨" };
+    setLogs([...logs, newLog]);
+  };
+
   const insertMoney = (price) => {
     const editedWalletMoney = walletMoney.map((coin) => {
-      if (coin.price === price) {
+      if (coin.price === price && coin.quantity > 0) {
         return { ...coin, quantity: coin.quantity - 1 };
       }
       return coin;
     });
 
     setWalletMoney(editedWalletMoney);
+    addInsertLog(price);
   };
 
   return (
