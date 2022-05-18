35208cdc0ce9bef5239ccd4b1963412d56c80087
decay95 <decay95@naver.com>
2022-05-18 11:32:05 +0900

    [fix] 충돌 해결문구 제거

--- src/index.js
+++ src/index.js
@@ -1,26 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-<<<<<<< HEAD
 import { BrowserRouter } from 'react-router-dom';
 import App from './App';
-=======
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import App from './App';
-import { Store } from './components/main/store';
->>>>>>> 85646caaee5539adea0f7e7fcec66ef9a42f16ea
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <BrowserRouter>
-<<<<<<< HEAD
       <App />
-=======
-      <Routes>
-        <Route path="/" element={<App />} />
-        <Route path="/store" element={<Store />} />
-      </Routes>
->>>>>>> 85646caaee5539adea0f7e7fcec66ef9a42f16ea
     </BrowserRouter>
   </React.StrictMode>
 );
