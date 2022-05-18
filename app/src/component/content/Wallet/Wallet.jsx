ce0bffa19564d01b573540d0a56d7a3bcc10d327
dpfla8123 <dpfla8123@gmail.com>
2022-05-18 14:50:00 +0900

    feat: 지갑 데이터 렌더링

--- app/src/component/content/Wallet/Wallet.jsx
+++ app/src/component/content/Wallet/Wallet.jsx
@@ -1,7 +1,41 @@
+import styled from 'styled-components';
+import { useContext } from 'react';
+import { MoneyContext } from '../../../index';
+
+const MONEY_TABLE_HEADER = {
+  UNIT: '단위',
+  COUNT: '개수',
+};
+
 function Wallet() {
+  const { money } = useContext(MoneyContext);
+  const moneyArr = Object.entries(money);
+
   return (
-    <div>지갑</div>
+    <Table>
+      <thead>
+        <tr>
+          <th>{MONEY_TABLE_HEADER.UNIT}</th>
+          <th>{MONEY_TABLE_HEADER.COUNT}</th>
+        </tr>
+      </thead>
+      <tbody>
+        {moneyArr.map(([unit, count]) => (
+          <tr key={unit}>
+            <td>{unit}원</td>
+            <td>
+              <input type="number" value={count} />
+            </td>
+          </tr>
+        ))}
+      </tbody>
+    </Table>
   );
 }
+
+const Table = styled.table`
+  margin: 20px auto 0;
+  width: 460px;
+`;
 
 export default Wallet;
--- app/src/index.js
+++ app/src/index.js
@@ -3,8 +3,10 @@ import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import App from './component/App';
 import products from './mock/products.json';
+import money from './mock/money.json';
 
 export const ProductsContext = createContext();
+export const MoneyContext = createContext();
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
@@ -12,7 +14,9 @@ root.render(
   <React.StrictMode>
     <BrowserRouter>
       <ProductsContext.Provider value={products}>
+      <MoneyContext.Provider value={money}>
         <App />
+      </MoneyContext.Provider>
       </ProductsContext.Provider>
     </BrowserRouter>
   </React.StrictMode>,
