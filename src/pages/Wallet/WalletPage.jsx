3b65ec4db3228a81c6553bb816eb52a093446a29
Hemudi <ksum1205@naver.com>
2022-05-18 19:32:44 +0900

    Feat: Wallet Page Component 구현
    Related to: #8

---
+++ src/pages/Wallet/WalletPage.jsx
@@ -0,0 +1,16 @@
+import VendingMachineController from 'components/organisms/VendingMachineController/VendingMachineController';
+import Wallet from 'components/organisms/Wallet/Wallet';
+import * as Styled from 'pages/Wallet/WalletPage.style';
+
+const WalletPage = () => {
+  return (
+    <Styled.WalletPage>
+      <Styled.WalletPageWrapper>
+        <VendingMachineController />
+        <Wallet />
+      </Styled.WalletPageWrapper>
+    </Styled.WalletPage>
+  );
+};
+
+export default WalletPage;
---
+++ src/pages/Wallet/WalletPage.stories.js
@@ -0,0 +1,8 @@
+import WalletPage from './WalletPage';
+
+export default {
+  title: 'Pages/WalletPage',
+  component: WalletPage,
+};
+
+export const DefaultWalletPage = () => <WalletPage />;
