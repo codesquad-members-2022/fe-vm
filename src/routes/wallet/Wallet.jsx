b3f771f7a9dc8ac212860e36dce49958a86fd7cf
nanjeong <whdwkxj7@naver.com>
2022-05-18 13:53:20 +0900

    feat: 지갑에서 금액을 클릭하면 지갑에서 돈을 꺼내 자판기에 넣도록 함

--- src/routes/wallet/Wallet.jsx
+++ src/routes/wallet/Wallet.jsx
@@ -1,3 +1,4 @@
+import { useVendingMachineContext } from "../../context/VendingMachineContext";
 import { useWalletContext } from "../../context/WalletContext";
 import {
     WalletContainer,
@@ -8,13 +9,33 @@ import {
 } from "./Wallet.Style";
 
 function Wallet() {
-    const { wallet, totalAmount } = useWalletContext();
+    const { wallet, totalAmount, updateWallet } = useWalletContext();
+    const { addRecord, putMoneyIntoVendingMachine } =
+        useVendingMachineContext();
+
+    const takeMoneyFromWallet = (moneyUnit) => {
+        const moneyInWallet = wallet.findIndex(
+            (money) => money.unit === moneyUnit
+        );
+
+        if (!moneyInWallet || !wallet[moneyInWallet].count) {
+            return;
+        }
+
+        wallet[moneyInWallet].count -= 1;
+        updateWallet(wallet);
+        addRecord(`${wallet[moneyInWallet].unit}원이 투입됨`);
+        putMoneyIntoVendingMachine(wallet[moneyInWallet].unit);
+    };
 
     return (
         <WalletContainer>
             {wallet.map((moneyUnit) => (
                 <WalletItemContainer key={moneyUnit.id}>
-                    <MoneyUnit disabled={!moneyUnit.count}>
+                    <MoneyUnit
+                        disabled={!moneyUnit.count}
+                        onClick={() => takeMoneyFromWallet(moneyUnit.unit)}
+                    >
                         {moneyUnit.unit}원
                     </MoneyUnit>
                     <WalletItem>{moneyUnit.count}개</WalletItem>
