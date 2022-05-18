028d20fc6797d56621dd49a19f2ff7ae5912bb2f
suuzin <8oozin@gmail.com>
2022-05-18 17:10:30 +0900

    feat : 지갑 금액 및 수량

---
+++ src/components/WalletMachine/WalletAmount.js
@@ -0,0 +1,6 @@
+import "./walletPrice.css";
+const WalletAmount = ({ amount }) => {
+  return <div className="walletprice-wrapper">{amount}개</div>;
+};
+
+export default WalletAmount;
---
+++ src/components/WalletMachine/WalletPrice.js
@@ -0,0 +1,6 @@
+import "./walletPrice.css";
+const WalletPrice = ({ price }) => {
+  return <div className="walletprice-wrapper">{price}원</div>;
+};
+
+export default WalletPrice;
