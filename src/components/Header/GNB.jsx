0f0640a31e2311c45fddef545ba29833e35f55d7
decay95 <decay95@naver.com>
2022-05-18 10:57:45 +0900

    [feat] Page 전환시 Header Slide 구현

--- src/components/Header/GNB.jsx
+++ src/components/Header/GNB.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
 import styled from 'styled-components';
-import { useNavigate } from 'react-router-dom';
-import { F_center, Default_shadow, Color } from 'Assets/Style/Common';
+import { useLocation, useNavigate } from 'react-router-dom';
+import { F_center, Color } from 'Assets/Style/Common';
 
 const GNB = () => {
+  const { pathname } = useLocation();
   const navigate = useNavigate();
 
   const VMBtnClickHandler = () => {
@@ -16,14 +17,26 @@ const GNB = () => {
   return (
     <form>
       <GNBdiv>
-        <GNBRadioButton type="radio" name="GNB" id="VMPageSelectBtn" />
+        <Slide className="slide" pathname={pathname} />
+        <GNBRadioButton
+          type="radio"
+          name="GNB"
+          id="VMPageSelectBtn"
+          checked={true}
+          readOnly
+        />
         <GNBLabel
           onClick={VMBtnClickHandler}
           htmlFor="VMPageSelectBtn"
           className="VMSelectBtn">
           자판기
         </GNBLabel>
-        <GNBRadioButton type="radio" name="GNB" id="WalletPageSelectBtn" />
+        <GNBRadioButton
+          type="radio"
+          name="GNB"
+          id="WalletPageSelectBtn"
+          readOnly
+        />
         <GNBLabel
           onClick={WalletBtnClickHandler}
           htmlFor="WalletPageSelectBtn"
@@ -35,21 +48,38 @@ const GNB = () => {
   );
 };
 
+const Slide = styled.div`
+  background-color: ${Color.yellowGreen};
+  position: absolute;
+  width: 20rem;
+  height: 6.9rem;
+  border-radius: 10rem;
+  transition: 0.3s;
+  box-shadow: 0 0.3rem 2rem rgba(79, 255, 181, 0.5);
+  ${({ pathname }) => {
+    if (pathname !== '/wallet') {
+      return 'left:50%;transform:translateX(-100%)';
+    }
+    return 'right: 0px';
+  }}
+`;
+
 const GNBdiv = styled.div`
   ${F_center};
+  $this: &;
+  margin-top: 3rem;
   justify-content: space-around;
+  position: relative;
   width: 40rem;
   height: 6.9rem;
-  background-color: ${Color.white};
-  ${Default_shadow}
+  background-color: ${Color.headerGray};
+  box-shadow: 0 0.4rem 3rem rgba(19, 15, 37, 0.1);
   border-radius: 10rem;
-  > #VMPageSelectBtn:checked ~ .VMSelectBtn {
-    transition: 1s;
-    background-color: ${Color.yellowGreen};
+  #VMPageSelectBtn:checked ~ .VMSelectBtn {
+    color: black;
   }
-  > #WalletPageSelectBtn:checked ~ .WalletSelectBtn {
-    transition: 1s;
-    background-color: ${Color.yellowGreen};
+  #WalletPageSelectBtn:checked ~ .WalletSelectBtn {
+    color: black;
   }
 `;
 
@@ -59,11 +89,13 @@ const GNBRadioButton = styled.input`
 
 const GNBLabel = styled.label`
   ${F_center}
+  color: #6E757D;
   width: 20rem;
   height: 6.9rem;
   border-radius: 10rem;
   font-size: 2.8rem;
   font-weight: 700;
   cursor: pointer;
+  z-index: 1;
 `;
 export default GNB;
