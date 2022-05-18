e53252eca2445a1442b2eea6e176744da21d2296
decay95 <decay95@naver.com>
2022-05-18 10:56:26 +0900

    [feat] VM Product Mokdata 구성

---
+++ src/Assets/VMData/products.js
@@ -0,0 +1,88 @@
+const VMproducts = [
+  {
+    name: '레인보우샤베트',
+    price: 2500,
+    img: '/img/rainbow.png',
+    stock: 2,
+    id: 'rainbow',
+  },
+  {
+    name: '환타',
+    price: 1000,
+    img: '/img/orange.png',
+    stock: 3,
+    id: 'fanta',
+  },
+  {
+    name: '에너지드링크',
+    price: 800,
+    img: '/img/soda.png',
+    stock: 10,
+    id: 'powerAde',
+  },
+  {
+    name: '초코우유',
+    price: 1000,
+    img: '/img/chocolate.png',
+    stock: 5,
+    id: 'chocolate',
+  },
+  {
+    name: '코코아주스',
+    price: 1200,
+    img: '/img/papaya.png',
+    stock: 4,
+    id: 'cocoa',
+  },
+  {
+    name: '오렌지주스',
+    price: 1300,
+    img: '/img/orange2.png',
+    stock: 4,
+    id: 'orangeJuice',
+  },
+  {
+    name: '딸기주스',
+    price: 1000,
+    img: '/img/strawJuice.png',
+    stock: 4,
+    id: 'strawberry',
+  },
+  {
+    name: '블루베리주스',
+    price: 1000,
+    img: '/img/grape.png',
+    stock: 4,
+    id: 'blueberry',
+  },
+  {
+    name: '요구르트',
+    price: 500,
+    img: '/img/yogurt.png',
+    stock: 15,
+    id: 'yogurt',
+  },
+  {
+    name: '아보카도주스',
+    price: 1500,
+    img: '/img/abokado.png',
+    stock: 4,
+    id: 'abocado',
+  },
+  {
+    name: '생수',
+    price: 500,
+    img: '/img/water.png',
+    stock: 15,
+    id: 'water',
+  },
+  {
+    name: '참이슬',
+    price: 4500,
+    img: '/img/soju.png',
+    stock: 100,
+    id: 'soju',
+  },
+];
+
+export default VMproducts;
