c940cfbcf9b13fe7f0f93e0a072843b71b02dc53
도비 <85747667+JiminKim-dev@users.noreply.github.com>
2022-05-18 19:44:41 +0900

    Chore: github pages 관련 세팅 추가 (#21)

--- src/Router.jsx
+++ src/Router.jsx
@@ -6,7 +6,7 @@ import NotFound from 'pages/NotFound';
 
 function Router() {
   return (
-    <BrowserRouter>
+    <BrowserRouter basename={process.env.PUBLIC_URL}>
       <Routes>
         <Route path="/" element={<Layout />}>
           <Route index element={<VendingMachine />} />
