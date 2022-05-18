7b65c1da4886da1cfa389aabf411b101e8438e34
suuzin <8oozin@gmail.com>
2022-05-18 16:47:19 +0900

    refactor : 반환버튼 컴포넌트 수정 및 context 적용

--- src/components/ReturnButton/ReturnButton.js
+++ src/components/ReturnButton/ReturnButton.js
@@ -1,7 +1,17 @@
 import "./ReturnButton.css";
-const ReturnButton = ({ onClick }) => {
+import { useContext } from "react";
+import { MessageContext } from "../../context/MessageContext";
+import { MoneyContext } from "../../context/MoneyContext";
+
+const ReturnButton = () => {
+  const { setMessage } = useContext(MessageContext);
+  const { setAccumulatedPrice } = useContext(MoneyContext);
+  const returnPrice = () => {
+    setAccumulatedPrice(0);
+    setMessage([]);
+  };
   return (
-    <button className="return-value" onClick={onClick}>
+    <button className="return-value" onClick={returnPrice}>
       반환
     </button>
   );
