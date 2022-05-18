640587419ced5f8df1eff735ff5f09f215cc0522
suuzin <8oozin@gmail.com>
2022-05-18 16:39:53 +0900

    refactor : vendingmachine 컴포넌트 수정

--- src/routes/VendingMachine.js
+++ src/routes/VendingMachine.js
@@ -1,61 +1,13 @@
 import React from "react";
-import { useState } from "react";
-import { data } from "../datas/data";
 import "./VendingMachine.css";
 import Product from "../components/Product/Product";
-import InputValue from "../components/Input/InputValue";
-import ReturnButton from "../components/ReturnButton/ReturnButton";
-import MessageView from "../components/MessageView/MessageView";
-import CurrentPrice from "../components/CurrentPrice/CurrentPrice";
+import Information from "../components/Information/Information";
 
 const VendingMachine = () => {
-  const [inputPrice, setInputPrice] = useState(undefined);
-  const [message, setMessage] = useState([]);
-  const [accumulatedPrice, setAccumulatedPrice] = useState(0);
-
-  const writePriceHandler = (event) => {
-    setInputPrice(Number(event.target.value));
-  };
-
-  const insertPriceHandler = (event) => {
-    event.preventDefault();
-    setMessage((prev) => [inputPrice, ...prev]);
-    setAccumulatedPrice(accumulatedPrice + inputPrice);
-    setInputPrice("");
-  };
-
-  const selectProductHandler = (event) => {
-    setMessage((prev) => [event.target.innerText, ...prev]);
-  };
-
-  const returnPrice = () => {
-    setAccumulatedPrice(0);
-    setMessage([]);
-  };
-
   return (
     <div className="vendingmachine-wrapper">
-      <div className="product-wrapper">
-        {data.map((v) => (
-          <Product
-            key={v.id}
-            name={v.name}
-            price={v.price}
-            accumulatedPrice={accumulatedPrice}
-            onClick={selectProductHandler}
-          />
-        ))}
-      </div>
-      <div className="vendingmachine-input-wrapper">
-        <CurrentPrice accumulatedPrice={accumulatedPrice} />
-        <InputValue
-          onSubmit={insertPriceHandler}
-          onChange={writePriceHandler}
-          value={inputPrice || ""}
-        />
-        <ReturnButton onClick={returnPrice} />
-        <MessageView message={message} />
-      </div>
+      <Product />
+      <Information />
     </div>
   );
 };
