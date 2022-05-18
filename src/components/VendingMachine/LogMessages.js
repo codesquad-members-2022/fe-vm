3715af02e0bc6cff5a49bb66ef0c5893f936d528
ldldz <qkrqudrjs4350@gmail.com>
2022-05-18 14:34:49 +0900

    Feat: 항상 마지막 로그 메세지 보여주기

--- src/components/VendingMachine/LogMessages.js
+++ src/components/VendingMachine/LogMessages.js
@@ -1,14 +1,19 @@
-import { useContext } from "react";
+import { useContext, useEffect, useRef } from "react";
 import styled from "styled-components";
 import { LogContext } from "../../contexts/Log";
 
 export default function LogMessages() {
   const { logMessages } = useContext(LogContext);
+  const messagesEndRef = useRef();
+  useEffect(() => {
+    messagesEndRef.current.scrollIntoView({ behavior: "auto" });
+  });
   return (
     <LogMessagesWrapper>
       {logMessages.map((logMessage, index) => (
         <LogMessage key={index}>{logMessage}</LogMessage>
       ))}
+      <div ref={messagesEndRef} />
     </LogMessagesWrapper>
   );
 }
@@ -20,7 +25,10 @@ const LogMessagesWrapper = styled.div`
   border-radius: 4px;
   width: 200px;
   height: 200px;
-  overflow: auto;
+  overflow: scroll;
   font-size: 12px;
+  ::-webkit-scrollbar {
+    display: none;
+  }
 `;
 const LogMessage = styled.div``;
