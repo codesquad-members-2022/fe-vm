6f3b28264d318a3e6adb0ca1b0415b214ca52db4
godhyeongman <aaaa4000@naver.com>
2022-05-18 16:33:41 +0900

    feat: 피드백 적용 dispatch 생성 함수 구현

--- src/Hooks/Account.jsx
+++ src/Hooks/Account.jsx
@@ -1,23 +1,18 @@
 import React, {useReducer} from 'react';
 
+import * as createDispatch from '../Reducer/AccountActions';
+
 export const useAccount = (initial, reducer) => {
   const [userMoney, dispatchUserMoney] = useReducer(reducer, initial);
 
-  const insertMoney = income => {
-    dispatchUserMoney({type: 'insert', incomeMoney: income});
-  };
+  const insertMoney = createDispatch.insertMoney(dispatchUserMoney);
 
-  const refundMoney = () => {
-    dispatchUserMoney({type: 'refund'});
-  };
+  const refundMoney = createDispatch.refundMoney(dispatchUserMoney);
 
-  const buyProduct = productPrice => () => {
-    dispatchUserMoney({type: 'buy', incomeMoney: productPrice});
-  };
+  const buyProduct = productPrice => () =>
+    createDispatch.buyProduct(dispatchUserMoney)(productPrice);
 
-  const inputMoney = income => {
-    dispatchUserMoney({type: 'input', incomeMoney: income});
-  };
+  const inputMoney = createDispatch.inputMoney(dispatchUserMoney);
 
   return {insertMoney, refundMoney, buyProduct, inputMoney, userMoney};
 };
---
+++ src/Reducer/AccountActions.js
@@ -0,0 +1,15 @@
+export const insertMoney = dispatchFn => income => {
+  dispatchFn({type: 'insert', incomeMoney: income});
+};
+
+export const refundMoney = dispatchFn => () => {
+  dispatchFn({type: 'refund'});
+};
+
+export const buyProduct = dispatchFn => productPrice => {
+  dispatchFn({type: 'buy', incomeMoney: productPrice});
+};
+
+export const inputMoney = dispatchFn => income => {
+  dispatchFn({type: 'input', incomeMoney: income});
+};
