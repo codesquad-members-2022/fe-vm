eeffde16c2238572f1a9bd55ea2aec8470b734f0
nanjeong <whdwkxj7@naver.com>
2022-05-18 16:46:10 +0900

    feat: 반환 버튼을 클릭하면 잔돈을 반환

--- src/routes/vendingMachine/VendingMachine.jsx
+++ src/routes/vendingMachine/VendingMachine.jsx
@@ -1,6 +1,8 @@
 import MoneySlot from "../../components/moneySlot/MoneySlot";
 import Products from "../../components/product/Products";
 import RecordBoard from "../../components/recordBoard/RecordBoard";
+import { useVendingMachineContext } from "../../context/VendingMachineContext";
+import { useWalletContext } from "../../context/WalletContext";
 import {
     VendingMachineContainer,
     UserInterfaceContainer,
@@ -8,12 +10,36 @@ import {
 } from "./VendingMachine.style";
 
 function VendingMachine() {
+    const {
+        addRecord,
+        moneyInVendingMachine,
+        totalMoneyInVendingMachine,
+        setMoneyInVendingMachine,
+    } = useVendingMachineContext();
+    const { wallet, updateWallet } = useWalletContext();
+
+    const returnMoney = () => {
+        if (!totalMoneyInVendingMachine) {
+            return;
+        }
+
+        Object.keys(moneyInVendingMachine).forEach((moneyUnit) => {
+            const moneyIndex = wallet.findIndex(
+                (money) => money.unit === Number(moneyUnit)
+            );
+            wallet[moneyIndex].count += moneyInVendingMachine[moneyUnit];
+        });
+        updateWallet(wallet);
+        addRecord(`잔돈 ${totalMoneyInVendingMachine}원 반환`);
+        setMoneyInVendingMachine({});
+    };
+
     return (
         <VendingMachineContainer>
             <Products />
             <UserInterfaceContainer>
                 <MoneySlot />
-                <ChangesButton>반환</ChangesButton>
+                <ChangesButton onClick={returnMoney}>반환</ChangesButton>
                 <RecordBoard />
             </UserInterfaceContainer>
         </VendingMachineContainer>
