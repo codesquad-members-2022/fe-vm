7ef708de116d8d46b52be67d5f8d20f8cafc4fd0
Hemudi <ksum1205@naver.com>
2022-05-18 21:54:55 +0900

    Feat: LogList Scroll 위치 변경 기능 구현
    Related to: #11

--- src/components/molecules/LogList/LogList.jsx
+++ src/components/molecules/LogList/LogList.jsx
@@ -1,11 +1,18 @@
+import React, { useEffect, useRef } from 'react';
 import mockData from './LogListMockData';
 import ListItem from 'components/atoms/ListItem/ListItem';
 import { LogListWrapper, StyledLogList } from './LogList.style';
 
 const LogList = () => {
+  const logListRef = useRef(null);
+
+  useEffect(() => {
+    logListRef.current.scrollTop = logListRef.current.scrollHeight;
+  }, []);
+
   return (
     <LogListWrapper>
-      <StyledLogList>
+      <StyledLogList ref={logListRef}>
         {mockData.map((v, i) => {
           return <ListItem key={v.id}>{getParsedLog(v.action, v.contents)}</ListItem>;
         })}
