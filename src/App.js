d14957681c59326ad91f6af9b8c37ebd391f90b9
suuzin <8oozin@gmail.com>
2022-05-18 16:31:57 +0900

    feat : App.js에 useContext.Provider 적용

--- src/App.js
+++ src/App.js
@@ -2,8 +2,15 @@ import { BrowserRouter, Route, Routes, NavLink } from "react-router-dom";
 import VendorMachine from "./routes/VendingMachine";
 import Wallet from "./routes/Wallet";
 import "./App.css";
+import { useState } from "react";
+import { InputContext } from "./context/InputContext";
+import { MessageContext } from "./context/MessageContext";
+import { MoneyContext } from "./context/MoneyContext";
 
 function App() {
+  const [inputPrice, setInputPrice] = useState("");
+  const [message, setMessage] = useState([]);
+  const [accumulatedPrice, setAccumulatedPrice] = useState(0);
   return (
     <BrowserRouter>
       <nav>
@@ -20,11 +27,16 @@ function App() {
           지갑
         </NavLink>
       </nav>
-
-      <Routes>
-        <Route path="/" element={<VendorMachine />} />
-        <Route path="/wallet" element={<Wallet />} />
-      </Routes>
+      <MoneyContext.Provider value={{ accumulatedPrice, setAccumulatedPrice }}>
+        <MessageContext.Provider value={{ message, setMessage }}>
+          <InputContext.Provider value={{ inputPrice, setInputPrice }}>
+            <Routes>
+              <Route path="/" element={<VendorMachine />} />
+              <Route path="/wallet" element={<Wallet />} />
+            </Routes>
+          </InputContext.Provider>
+        </MessageContext.Provider>
+      </MoneyContext.Provider>
     </BrowserRouter>
   );
 }
