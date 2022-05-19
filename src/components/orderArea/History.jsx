3a5ba42f5d30d10022292744467b3f385475b043
jindonyy <rsefa94@gmail.com>
2022-05-20 06:49:11 +0900

    Feat: 히스토리 젤 하단으로 자동 스크롤 기능 구현
    closed issue #14

--- src/components/orderArea/History.jsx
+++ src/components/orderArea/History.jsx
@@ -1,12 +1,17 @@
-import React, { useContext } from 'react';
+import React, { useEffect, useContext, useRef } from 'react';
 import { HistoryList, HistoryItem } from 'components/orderArea/History.style';
 import { HistoryContext } from 'contexts/HistoryProvider';
 
 export default function History() {
   const historyList = useContext(HistoryContext);
+  const historyContainer = useRef(null);
+
+  useEffect(() => {
+    historyContainer.current.scrollTop = historyContainer.current.scrollHeight;
+  }, [historyList]);
 
   return (
-    <HistoryList>
+    <HistoryList ref={historyContainer}>
       {historyList.map((item, idx) => (
         <HistoryItem key={`history_${idx}`}>{item}</HistoryItem>
       ))}
