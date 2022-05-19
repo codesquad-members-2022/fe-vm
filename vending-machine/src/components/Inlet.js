fe0695b70092b48eb841fabd897aeb72b0e57851
SeungHyun <fm10033@gmail.com>
2022-05-19 16:15:29 +0900

    Feat : input값 유효성 검사(숫자인지 아닌지)구현

--- vending-machine/src/components/Inlet.js
+++ vending-machine/src/components/Inlet.js
@@ -1,10 +1,29 @@
-import React from "react";
+import { fireEvent } from "@testing-library/react";
+import React, { useState } from "react";
 import styled from "styled-components";
 import { HeightSort } from "../style/Globalstyles";
 function Inlet() {
+  const [input, setInput] = useState("");
+  function checkInput(text) {
+    console.log(typeof text);
+    if (isNaN(text)) {
+      alert("유효한 입력값이 아닙니다 다시 입력해 주세요");
+      return false;
+    } else {
+      return true;
+    }
+  function submitValue(e) {
+    if (e.key === "Enter") {
+      if (!checkInput(e.target.value)) {
+        e.target.value = null;
+      }
+    }
+  }
   return (
     <InletWrap>
-      <InletInput></InletInput>
+      <InletInputWrap>
+        <InletInput onKeyPress={submitValue}></InletInput>
+      </InletInputWrap>
     </InletWrap>
   );
 }
@@ -15,13 +34,19 @@ const InletWrap = styled.div`
   justify-content: flex-end;
   box-sizing: border-box;
   padding: 5%;
+  gap: 10px;
 `;
-const InletInput = styled.input`
+const InletInputWrap = styled.div`
   width: 30%;
   height: 100%;
   background: ${({ theme }) => theme.colors.darkGray};
+  border-radius: 5px;
+`;
+const InletInput = styled.input`
+  width: 100%;
+  height: 100%;
+  background: none;
   border: none;
   color: ${({ theme }) => theme.colors.white};
-  border-radius: 5px;
 `;
 export default Inlet;
