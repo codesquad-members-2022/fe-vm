d048101e3013cf7faf4b15adcddb61970d3833e8
jindonyy <rsefa94@gmail.com>
2022-05-20 00:28:02 +0900

    refactor: ProductsProvider 삭제

--- src/pages/VendingMachine.jsx
+++ src/pages/VendingMachine.jsx
@@ -2,18 +2,15 @@ import React from 'react';
 import ProductsArea from 'components/productsArea/ProductsArea';
 import OrderArea from 'components/orderArea/OrderArea';
 import { Container } from 'pages/VendingMachine.style';
-import { ProductsProvider } from 'Context/ProductsProvider';
 import { FinalPayProvider } from 'Context/FinalPayProvider';
 
 export default function VendingMachine() {
   return (
-    <ProductsProvider>
-      <FinalPayProvider>
-        <Container>
-          <ProductsArea />
-          <OrderArea />
-        </Container>
-      </FinalPayProvider>
-    </ProductsProvider>
+    <FinalPayProvider>
+      <Container>
+        <ProductsArea />
+        <OrderArea />
+      </Container>
+    </FinalPayProvider>
   );
 }
