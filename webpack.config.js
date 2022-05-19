06ee7d48406a199b61f794ebaf215b281dc32070
godhyeongman <aaaa4000@naver.com>
2022-05-19 17:24:58 +0900

    chore: histroyApiFallback 설정 추가

--- webpack.config.js
+++ webpack.config.js
@@ -43,6 +43,7 @@ module.exports = {
     publicPath: './',
   },
   devServer: {
+    historyApiFallback: true,
     devMiddleware: {publicPath: '/'}, // devMiddleware의 publicPath는  빌드된 파일 넣는곳
     static: {directory: path.resolve(__dirname, 'public')}, // static은 빌드되기 전 파일이 있는 곳
     hot: true,
