bcd2ba6a0a1691c7e79a8d96f1bfce0db361e5f7
yongseongjeon <yongseong.jeon95@gmail.com>
2022-05-19 17:19:34 +0900

    ✨ 투입된 금액이 만족되면 구매 가능한 상품 테두리 색상 빨간색으로 변경

--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -1,10 +1,13 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import styled from 'styled-components';
+import { MoneyContext } from 'components/App';
 
 function Product({ name, price }) {
+  const { curMoney } = useContext(MoneyContext);
+  const canPurchase = curMoney >= price;
   return (
     <div>
-      <Name>{name}</Name>
+      <Name canPurchase={canPurchase}>{name}</Name>
       <Price>{price}</Price>
     </div>
   );
@@ -12,12 +15,16 @@ function Product({ name, price }) {
 
 export default Product;
 
-const Name = styled.div({
-  border: '1px solid black',
-  height: '50px',
-  textAlign: 'center',
-  lineHeight: '50px',
-});
-const Price = styled.div({
-  textAlign: 'center',
-});
+const Name = styled.button`
+  border-color: ${({ canPurchase }) => (canPurchase ? 'red' : 'black')};
+  width: 100px;
+  height: 50px;
+  padding: 0;
+  text-align: center;
+  line-height: 50px;
+  cursor: pointer;
+`;
+
+const Price = styled.div`
+  text-align: center;
+`;
