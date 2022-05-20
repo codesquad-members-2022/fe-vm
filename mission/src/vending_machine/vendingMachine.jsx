b490eefd674f98e65dceb5807e2d3865611453e4
onlinekkabchi <66970178+onlinekkabchi@users.noreply.github.com>
2022-05-20 18:20:39 +0900

    Revert "[sonya] 2주차 금요일 PR"

--- mission/src/vending_machine/vendingMachine.jsx
+++ mission/src/vending_machine/vendingMachine.jsx
@@ -1,203 +1,67 @@
-import React, { useState, useEffect, useReducer } from "react";
+import React, { useState, useEffect } from "react";
+import styled from "styled-components";
+import "./vendingMachine.css";
 import { menu } from "./menu.js";
-import {
-    VendingMachineDiv,
-    VendingMachineMenu,
-    VendingMachineDrink,
-    VendingMachineInsertCoin,
-    VendingMachineInputBox,
-    VendingMachineInputTitle,
-    VendingMachineInsertTotal,
-    VendingMachineInputReturnBtn,
-    VendingMachineInputLog,
-} from "./vendingMachine_styled.jsx";
 
-function Drink({ index, name, price, amount, choiceLog }) {
-    const readyDrink = {
-        index: index,
-        name: name,
-        price: price,
-        amount: amount,
-        sellAvailable: false,
-        soldOut: false,
-    };
+const VendingMachineMenuDrink = styled.div`
+    background-color: aquamarine;
+    width:40px;
+    margin: 5px;
+    cursor:pointer;
+`;
 
-    const reducer = (state, action) => {
-        switch (action.type) {
-            case "sellDrink":
-                choiceLog(name);
-                return {
-                    index: index,
-                    name: name,
-                    price: price,
-                    amount: state.amount - 1,
-                    sellAvailable: false,
-                    soldOut: false,
-                };
-            default:
-                throw new Error();
+function DrinkMenu({ index, name, price, amount, soldout }) {
+    const [count, setCount] = useState(amount);
+    const [close, setClose] = useState(soldout);
+
+    useEffect(()=>{
+        if(count === 0){
+            setClose(true);
+        } else if (count>0){
+            setClose(false);
         }
-    };
-
-    const [drink, dispatch] = useReducer(reducer, readyDrink);
-
-    useEffect(() => {
-        if (readyDrink.amount === 0) {
-            readyDrink.soldOut = true;
-        }
-    });
+    })
 
     return (
-        <>
-            <VendingMachineDrink
-                onClick={() => dispatch({ type: "sellDrink" })}
-            >
-                <p className="drink-name">{drink.name}</p>
-                <p className="drink-price">{drink.price}</p>
-                <p className="drink-amount">수량 : {drink.amount}</p>
-            </VendingMachineDrink>
-        </>
-    );
-}
-
-function VendingMachineInputForm({ addValue }) {
-    const [userInput, setUserInput] = useState("");
-
-    const handleChange = (e) => {
-        setUserInput(e.currentTarget.value);
-    };
-
-    const keyPress = (e) => {
-        if (e.keyCode === 13) {
-            e.preventDefault();
-            console.log(userInput);
-            addValue(userInput);
-            setUserInput("");
-        }
-    };
-
-    return (
-        <VendingMachineInsertCoin
-            onChange={handleChange}
-            onKeyDown={keyPress}
-            type="text"
-            value={userInput}
-            placeholder="동전을 투입하세요!"
-        />
-
+        <VendingMachineMenuDrink onClick={() => {
+                setCount(count - 1);
+                console.log(count);
+            }}>
+            <p className="vending-machine__menu__drink--name">{name}</p>
+            <p className="vending-machine__menu__drink--price">{price}</p>
+            <p className="vending-machine__menu__drink--amount">{amount}</p>
+        </VendingMachineMenuDrink>
     );
 }
 
 function VendingMachine() {
     const [drink, setDrink] = useState([]);
-    const [total, setTotal] = useState(0);
-    const [logs, setLog] = useState([
-        { text: "0원이 투입되었습니다.", money: "0" },
-    ]);
-    const throttlingButton = {
-        timerButton: false,
-    };
 
-    const returnTimer = new Promise((res) => {
-        res(payback());
-        setTimeout(() => {
-            const newLog = [
-                ...logs,
-                {
-                    text: `${total}이 반환되었습니다.`,
-                    money: "0",
-                },
-            ];
-            setLog(newLog);
-            setTotal(0);
-        }, 5000);
-        console.log("timer");
-    });
-
-    const payback = await returnTimer
-
-    const insertSlot = (inputMoney) => {
-        throttlingButton.timerButton = true; // 동전투입하면 반환타이머 작동.
-        const newLog = [
-            ...logs,
-            {
-                text: `${inputMoney}원이 투입되었습니다.`,
-                money: inputMoney,
-            },
-        ];
-        setLog(newLog);
-        calculateCoin(newLog);
-        console.log(newLog);
-    };
-
-    const chooseDrink = (drink) => {
-        const newLog = [
-            ...logs,
-            {
-                text: `${drink}를 선택하셨습니다.`,
-                money: "0",
-            },
-        ];
-        setLog(newLog);
-        console.log(newLog);
-    };
-
-    const calculateCoin = (c) => {
-        let sum = 0;
-        const numArr = c.map((e) => Number(e.money));
-        for (const i of numArr) {
-            sum += i;
-        }
-        console.log(sum);
-        setTotal(sum);
-        return sum;
-    };
-
-    useEffect(() => {
+    useEffect(()=>{
         setDrink(menu);
-        if (total === 0) {
-            throttlingButton.timerButton = false;
-        }
-        if (throttlingButton.timerButton === false) {
-            clearTimeout(returnTimer);
-            console.log("returnTimer canceled!");
-        }
-    });
+    })
 
     return (
-        <>
-            <VendingMachineDiv>
-                <VendingMachineMenu>
-                    {drink.map(({ i, name, price, amount }) => (
-                        <Drink
-                            index={i}
-                            name={name}
-                            price={price}
-                            amount={amount}
-                            choiceLog={chooseDrink}
-                        />
-                    ))}
-                </VendingMachineMenu>
-                <VendingMachineInputBox>
-                    <VendingMachineInputTitle>
-                        결제가 힘든 자판기
-                    </VendingMachineInputTitle>
-                    <VendingMachineInsertTotal>
-                        투입금액: {total}
-                    </VendingMachineInsertTotal>
-                    <VendingMachineInputForm addValue={insertSlot} />
-                    <VendingMachineInputReturnBtn onClick={returnTimer}>
-                        반환
-                    </VendingMachineInputReturnBtn>
-                    <VendingMachineInputLog>
-                        {logs.map((l, index) => (
-                            <p key={index}>{l.text}</p>
-                        ))}
-                    </VendingMachineInputLog>
-                </VendingMachineInputBox>
-            </VendingMachineDiv>
-        </>
-
+        <div className="vending-machine">
+            <div className="vending-machine__menu">
+                {drink.map(({ name, price, amount }) => (
+                    <DrinkMenu name={name} price={price} amount={amount} />
+                ))}
+            </div>
+            <div className="vending-machine__input">
+                <input
+                    classsName="vending-machine__input__money"
+                    type="text"
+                    placeholder="동전을 투입하세요."
+                />
+                <button className="vending-machine__input__return-btn">
+                    반환
+                </button>
+                <div className="vending-machine__input__log">
+                    500원이 투입되엇음.
+                </div>
+            </div>
+        </div>
     );
 }
 
