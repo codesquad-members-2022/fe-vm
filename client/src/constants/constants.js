b7427b7f09f1439d564a4a924eae737f0ca93dc0
ver <luzverde0314@gmail.com>
2022-05-20 14:00:12 +0900

    Feat: 타이머 상수로 관리

--- client/src/constants/constants.js
+++ client/src/constants/constants.js
@@ -1,2 +1,4 @@
 export const moneyUnitArr = [10000, 5000, 1000, 500, 100, 50, 10];
 export const reversedMoneyUnitArr = [10, 50, 100, 500, 1000, 5000, 10000];
+export const SELECT_COUNT = 2000;
+export const RETURN_COUNT = 5000;
