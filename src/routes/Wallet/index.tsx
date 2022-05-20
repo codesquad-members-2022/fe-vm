3d8100b41928dbafa3c6d62c771d3d15b435b9f9
Cola <bcad1591@gmail.com>
2022-05-20 12:44:10 +0900

    Refactor: Props 변경중

--- src/routes/Wallet/index.tsx
+++ src/routes/Wallet/index.tsx
@@ -1,9 +1,9 @@
 import React, { memo } from 'react';
 
 import { RETURN_MONEY_DELAY } from '@/constants/timer';
-import { LOG_ACTION, useLog } from '@/Context/VMContext/LogContext';
-import { MACHINE_ACTION, useMachine } from '@/Context/VMContext/MachineContext';
-import { ICoin, useWallet, WALLET_ACTION } from '@/Context/VMContext/WalletContext';
+import { LOG_ACTION, LogDispatch, useLog } from '@/Context/VMContext/LogContext';
+import { MACHINE_ACTION, MachineDispatch, useMachine } from '@/Context/VMContext/MachineContext';
+import { ICoin, useWallet, WALLET_ACTION, WalletDispatch } from '@/Context/VMContext/WalletContext';
 import { useTimer } from '@/hooks/useTimer';
 
 import * as S from './styles';
@@ -11,19 +11,28 @@ import * as S from './styles';
 interface CoinProps {
   coin: ICoin;
   index: number;
+  walletDispatch: WalletDispatch;
+  logDispatch: LogDispatch;
 }
 
 const Wallet = () => {
   const {
-    wallet: { state: walletState },
+    wallet: { state: walletState, dispatch: walletDispatch },
   } = useWallet();
+  const { dispatch: logDispatch } = useLog();
 
   return (
     <S.WalletLayout>
       <S.WalletLayer>
         <S.CoinList>
           {walletState.coins.map((coin, index) => (
-            <Coin key={coin.id} coin={coin} index={index} />
+            <Coin
+              key={coin.id}
+              coin={coin}
+              index={index}
+              walletDispatch={walletDispatch}
+              logDispatch={logDispatch}
+            />
           ))}
         </S.CoinList>
         <S.Balance>총 {walletState.balance.toLocaleString()}원</S.Balance>
@@ -32,14 +41,10 @@ const Wallet = () => {
   );
 };
 
-const Coin = memo(({ coin, index }: CoinProps) => {
+const Coin = memo(({ coin, index, walletDispatch, logDispatch }: CoinProps) => {
   const { amount, count } = coin;
-  const {
-    wallet: { dispatch: walletDispatch },
-  } = useWallet();
-  const { state: machineState, dispatch: machineDispatch } = useMachine();
-  const { state: logState, dispatch: logDispatch } = useLog();
   const [setTimer, clearTimer] = useTimer('insertMoney');
+  const { state: machineState, dispatch: machineDispatch } = useMachine();
 
   const onClickInsertButton = () => {
     if (count === 0) {
@@ -53,22 +58,12 @@ const Coin = memo(({ coin, index }: CoinProps) => {
     walletDispatch({ type: WALLET_ACTION.INSERT_COIN, payload: { amount, count, index } });
     machineDispatch({ type: MACHINE_ACTION.INSERT_MONEY, payload: { amount } });
     logDispatch({ type: LOG_ACTION.INSERT_MONEY, payload: { amount } });
-    setTimer(() => {
-      const returnAmount = machineState.totalInputAmount + amount;
-      machineDispatch({ type: MACHINE_ACTION.RETURN_MONEY });
-      // walletDispatch({ type: WALLET_ACTION.RETURN_COINS})
-      logDispatch({ type: LOG_ACTION.RETURN_MONEY, payload: { amount: returnAmount } });
-    }, RETURN_MONEY_DELAY);
-    // dispatch({
-    //   type: ACTION.SET_TIMER,
-    //   payload: {
-    //     key: 'returnChange',
-    //     delay: RETURN_CHANGE_DELAY,
-    //     callback: () => {
-    //       dispatch({ type: ACTION.RETURN_CHANGE });
-    //     },
-    //   },
-    // });
+    // setTimer(() => {
+    //   const returnAmount = machineState.totalInputAmount + amount;
+    //   machineDispatch({ type: MACHINE_ACTION.RETURN_MONEY });
+    //   // walletDispatch({ type: WALLET_ACTION.RETURN_COINS });
+    //   logDispatch({ type: LOG_ACTION.RETURN_MONEY, payload: { amount: returnAmount } });
+    // }, RETURN_MONEY_DELAY);
   };
 
   const onClickIncrementButton = () => {
