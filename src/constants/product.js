c99814509bff5ff7fe64853b0581d2eee7219fcf
soralee <sora2821@gmail.com>
2022-05-20 15:49:01 +0900

    Feat: money와 product 상수 파일 변경 및 생성

---
+++ src/constants/product.js
@@ -0,0 +1,18 @@
+import guid from 'util/guid';
+
+const PRODUCTS = [
+  { id: guid(), icon: '🐙', price: 100, stock: 5 },
+  { id: guid(), icon: '🦑', price: 200, stock: 5 },
+  { id: guid(), icon: '🦞', price: 300, stock: 5 },
+  { id: guid(), icon: '🦀', price: 400, stock: 5 },
+  { id: guid(), icon: '🦐', price: 500, stock: 5 },
+  { id: guid(), icon: '🦪', price: 1000, stock: 5 },
+  { id: guid(), icon: '🐠', price: 2000, stock: 5 },
+  { id: guid(), icon: '🐟', price: 3000, stock: 5 },
+  { id: guid(), icon: '🐡', price: 4000, stock: 5 },
+  { id: guid(), icon: '🐬', price: 5000, stock: 5 },
+  { id: guid(), icon: '🦈', price: 10000, stock: 5 },
+  { id: guid(), icon: '🦭', price: 100000, stock: 5 },
+];
+
+export default PRODUCTS;
