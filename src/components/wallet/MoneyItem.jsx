8c5c3bdad3800e528933ef1d9bd530e4863e9ca1
jindonyy <rsefa94@gmail.com>
2022-05-20 15:00:40 +0900

    Feat: 돈 클릭 시, 수량 감소 이벤트 구현
    issue #15

--- src/components/wallet/MoneyItem.jsx
+++ src/components/wallet/MoneyItem.jsx
@@ -3,10 +3,10 @@ import PropTypes from 'prop-types';
 import { Container, MoneyBtn, MoneyQuantity } from 'components/wallet/MoneyItem.style';
 import { addCommasToNumber } from 'utils/util';
 
-export default function QuantityBtnWrap({ info }) {
+export default function QuantityBtnWrap({ info, coin, decreaseQuantity }) {
   return (
     <Container>
-      <MoneyBtn color={info.color} coin={info.coin}>
+      <MoneyBtn color={info.color} coin={coin} onClick={() => decreaseQuantity(info.id)} disabled={info.quantity <= 0}>
         {addCommasToNumber(info.unit)}
       </MoneyBtn>
       <MoneyQuantity>{info.quantity}</MoneyQuantity>
@@ -19,9 +19,10 @@ QuantityBtnWrap.propTypes = {
     id: PropTypes.number,
     unit: PropTypes.number,
     quantity: PropTypes.number,
-    color: PropTypes.string,
-    coin: PropTypes.bool
-  })
+    color: PropTypes.string
+  }),
+  coin: PropTypes.bool,
+  decreaseQuantity: PropTypes.func
 };
 
 QuantityBtnWrap.defaultProps = {
@@ -29,7 +30,8 @@ QuantityBtnWrap.defaultProps = {
     id: null,
     unit: 0,
     quantity: 0,
-    color: '#fff',
-    coin: false
-  }
+    color: '#fff'
+  },
+  coin: false,
+  decreaseQuantity: () => {}
 };
--- src/pages/Wallet.jsx
+++ src/pages/Wallet.jsx
@@ -1,17 +1,22 @@
 import React, { useContext } from 'react';
 import { Container, MoneyList } from 'pages/Wallet.style';
-import { WalletContext } from 'contexts/WalletProvider';
+import { WalletContext, WalletSetContext } from 'contexts/WalletProvider';
 import MoneyItem from 'components/wallet/MoneyItem';
 import AmountWrap from 'components/wallet/AmountWrap';
 
 export default function Wallet() {
-  const walletState = useContext(WalletContext);
+  const [walletState, setWalletState] = [useContext(WalletContext), useContext(WalletSetContext)];
+
+  const decreaseQuantity = id => {
+    const newWalletState = walletState.map(item => (item.id === id ? { ...item, quantity: item.quantity - 1 } : item));
+    setWalletState(newWalletState);
+  };
 
   return (
     <Container>
       <MoneyList>
         {walletState.map(info => (
-          <MoneyItem key={info.id} info={info} coin={info.unit < 1000} />
+          <MoneyItem key={info.id} info={info} coin={info.unit < 1000} decreaseQuantity={decreaseQuantity} />
         ))}
       </MoneyList>
       <AmountWrap />
