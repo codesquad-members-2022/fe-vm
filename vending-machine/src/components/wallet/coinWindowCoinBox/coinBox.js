91d1e48bd02533d7e27f5780cc5c1cbf712efffb
bangdler <zbthz90@gmail.com>
2022-05-20 17:09:46 +0900

    feat: 코인 클릭시 자판기 입력금액에추가 및 로그 표시

--- vending-machine/src/components/wallet/coinWindowCoinBox/coinBox.js
+++ vending-machine/src/components/wallet/coinWindowCoinBox/coinBox.js
@@ -2,14 +2,25 @@ import { getWonTemplate } from '../../../helper/utils';
 import { StyledCoinBoxWrapper, StyledCoinBox } from './coinBox.styled';
 import { WalletContext } from '../../../context/walletProvider';
 import { useContext } from 'react';
+import { InputMoneyContext } from '../../../context/inputMoneyProvider';
+import { LogContext } from '../../../context/logProvider';
 
 export function CoinBox({ coinInfo }) {
   const { decrementCoin } = useContext(WalletContext);
+  const { setInputMoney } = useContext(InputMoneyContext);
+  const { setLogList } = useContext(LogContext);
 
   function handleClick() {
     if (coinInfo.quantity > 0) {
       decrementCoin(coinInfo.coin);
+      setInputMoney(inputMoney => inputMoney + coinInfo.coin);
+      logInputMoney(coinInfo.coin);
     }
+  }
+
+  function logInputMoney(currentInputMoney) {
+    const log = `${getWonTemplate(currentInputMoney)} 투입됨.`;
+    setLogList(logList => [...logList, log]);
   }
 
   return (
