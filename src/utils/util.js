629e10417bb46c27a8c6db97a7c6a37b7caf5c3b
jindonyy <rsefa94@gmail.com>
2022-05-20 14:45:37 +0900

    Refactor: util에서 delay 함수 삭제

--- src/utils/util.js
+++ src/utils/util.js
@@ -1,10 +1,3 @@
 const addCommasToNumber = number => number.toLocaleString('en');
 
-const delay = time =>
-  new Promise(resolve => {
-    setTimeout(() => {
-      resolve(time);
-    }, time);
-  });
-
-export { addCommasToNumber, delay };
+export { addCommasToNumber };
