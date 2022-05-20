b42d40fe6364f231756a11922fb224c4ebcbbcb2
jindonyy <rsefa94@gmail.com>
2022-05-21 00:06:11 +0900

    Refactor: FinalProvider App으로 이동

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import VendingMachine from 'pages/VendingMachine';
 import Wallet from 'pages/Wallet';
 import { Container } from 'App.style';
 import { WalletProvider } from 'contexts/WalletProvider';
+import { FinalPayProvider } from 'contexts/FinalPayProvider';
 import { SelectedProductProvider } from 'contexts/SelectedProductProvider';
 import { HistoryProvider } from 'contexts/HistoryProvider';
 import { VMTimerProvider } from 'contexts/VMTimerProvider';
@@ -12,21 +13,23 @@ import { VMTimerProvider } from 'contexts/VMTimerProvider';
 export default function App() {
   return (
     <WalletProvider>
-      <SelectedProductProvider>
-        <HistoryProvider>
-          <VMTimerProvider>
-            <Container>
-              <BrowserRouter basename={process.env.PUBLIC_URL}>
-                <GNB />
-                <Routes>
-                  <Route path="/" element={<VendingMachine />} />
-                  <Route path="/wallet" element={<Wallet />} />
-                </Routes>
-              </BrowserRouter>
-            </Container>
-          </VMTimerProvider>
-        </HistoryProvider>
-      </SelectedProductProvider>
+      <FinalPayProvider>
+        <SelectedProductProvider>
+          <HistoryProvider>
+            <VMTimerProvider>
+              <Container>
+                <BrowserRouter basename={process.env.PUBLIC_URL}>
+                  <GNB />
+                  <Routes>
+                    <Route path="/" element={<VendingMachine />} />
+                    <Route path="/wallet" element={<Wallet />} />
+                  </Routes>
+                </BrowserRouter>
+              </Container>
+            </VMTimerProvider>
+          </HistoryProvider>
+        </SelectedProductProvider>
+      </FinalPayProvider>
     </WalletProvider>
   );
 }
--- src/pages/Wallet.jsx
+++ src/pages/Wallet.jsx
@@ -1,11 +1,17 @@
 import React, { useContext } from 'react';
 import { Container, MoneyList } from 'pages/Wallet.style';
+import { addCommasToNumber } from 'utils/util';
 import { WalletContext, WalletSetContext } from 'contexts/WalletProvider';
 import MoneyItem from 'components/wallet/MoneyItem';
 import AmountWrap from 'components/wallet/AmountWrap';
 
 export default function Wallet() {
   const [walletState, setWalletState] = [useContext(WalletContext), useContext(WalletSetContext)];
+
+  const getAmount = () => {
+    const amount = walletState.reduce((acc, { unit, quantity }) => acc + unit * quantity, 0);
+    return addCommasToNumber(amount);
+  };
 
   const decreaseQuantity = id => {
     const newWalletState = walletState.map(item => (item.id === id ? { ...item, quantity: item.quantity - 1 } : item));
@@ -19,7 +25,7 @@ export default function Wallet() {
           <MoneyItem key={info.id} info={info} coin={info.unit < 1000} decreaseQuantity={decreaseQuantity} />
         ))}
       </MoneyList>
-      <AmountWrap />
+      <AmountWrap amount={getAmount()} />
     </Container>
   );
 }
