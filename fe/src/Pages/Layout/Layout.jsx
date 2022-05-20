b305ee9bcfb70ec6ceec049a99d5fa9e6b28dc3c
youryu0212 <youryu0212@naver.com>
2022-05-20 12:14:08 +0900

    feat: 주문 도중에 추가 충전 금지

--- fe/src/Pages/Layout/Layout.jsx
+++ fe/src/Pages/Layout/Layout.jsx
@@ -1,12 +1,27 @@
-import { NAV_MENUS } from 'Helper/constant';
-import { NavLink, Outlet } from 'react-router-dom';
-import { LayoutContainer, NavContainer, NavLi, NavUl } from './Layout.styled';
+import { OrderInProgressContext } from "Context/OrderInProgressProvider";
+import { NAV_MENUS } from "Helper/constant";
+import useSetAlertMessage from "Hooks/useSetAlertMessage";
+import { useContext } from "react";
+import { NavLink, Outlet } from "react-router-dom";
 
 export default function Layout() {
+  const orderInProgress = useContext(OrderInProgressContext);
+  const applyAlertMessage = useSetAlertMessage();
+
+  const handleClick = (event) => {
+    if (!orderInProgress) {
+      return;
+    }
+
+    applyAlertMessage("buying");
+
+    event.preventDefault();
+  };
+
   const navigations = NAV_MENUS.map(({ id, name, path }) => {
     return (
       <NavLi key={id} flex justify="center" align="center">
-        <NavLink to={path}>
+        <NavLink to={path} onClick={handleClick}>
           <div>{name}</div>
         </NavLink>
       </NavLi>
