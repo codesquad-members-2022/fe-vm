82fdf98f431537758ec66ada01efefae01f45113
soralee <sora2821@gmail.com>
2022-05-20 15:56:34 +0900

    Feat: ProductProvider 반영

--- src/App.js
+++ src/App.js
@@ -2,18 +2,21 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Layout from 'components/organisms/Layout/Layout';
 import VendingMachine from 'routes/VendingMachine/VendingMachine';
 import Wallet from 'routes/Wallet/Wallet';
-import { MoneyProvider } from 'components/atoms/Context/MoneyContext';
+import { MoneyProvider } from 'context/MoneyContext';
+import { ProductProvider } from 'context/ProductContext';
 
 function App() {
   return (
     <BrowserRouter>
       <MoneyProvider>
-        <Routes>
-          <Route path="/" element={<Layout />}>
-            <Route path="vendingMachine" element={<VendingMachine />} />
-            <Route path="wallet" element={<Wallet />} />
-          </Route>
-        </Routes>
+        <ProductProvider>
+          <Routes>
+            <Route path="/" element={<Layout />}>
+              <Route path="vendingMachine" element={<VendingMachine />} />
+              <Route path="wallet" element={<Wallet />} />
+            </Route>
+          </Routes>
+        </ProductProvider>
       </MoneyProvider>
     </BrowserRouter>
   );
