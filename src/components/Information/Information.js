b0bea42a610b02c26d71e8c2c3b17eb8d58ea6c2
suuzin <8oozin@gmail.com>
2022-05-20 14:31:56 +0900

    style : css수정

--- src/components/Information/Information.js
+++ src/components/Information/Information.js
@@ -2,9 +2,10 @@ import InputValue from "../Input/InputValue";
 import ReturnButton from "../ReturnButton/ReturnButton";
 import MessageView from "../MessageView/MessageView";
 import CurrentPrice from "../CurrentPrice/CurrentPrice";
+import "./Information.css";
 const Information = () => {
   return (
-    <div>
+    <div className="information-wrapper">
       <CurrentPrice />
       <InputValue />
       <ReturnButton />
