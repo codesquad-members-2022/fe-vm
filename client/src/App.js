a4ca2fc19d2645973820962309e1924e6c4c565e
ver <luzverde0314@gmail.com>
2022-05-20 13:59:33 +0900

    Feat: 전역에서 사용 될 타이머 컨텍스트 구현

--- client/src/App.js
+++ client/src/App.js
@@ -7,27 +7,30 @@ import InputStore from "store/InputStore";
 import MessageStore from "store/MessageStore";
 import WalletStore from "store/WalletStore";
 import MenuStore from "store/MenuStore";
+import TimerStore from "store/TimerStore";
 
 function App() {
   return (
     <BrowserRouter>
-      <NavBar />
+      <TimerStore>
+        <NavBar />
 
-      <WalletStore>
-        <InputStore>
-          <MessageStore>
-            <MenuStore>
-              <Routes>
-                <Route path="/" element={<VM />} />
-                <Route path="/wallet" element={<Wallet />} />
-                {/* TODO: 없는 페이지 들어갔을 때 NotFound 페이지 출력 후 몇 초 후 홈으로 리다이렉트 되는 식으로 처리 */}
-                {/* <Route path="/*" element={<NotFound />} /> */}
-                <Route path="*" element={<Navigate to="/" replace />} />
-              </Routes>
-            </MenuStore>
-          </MessageStore>
-        </InputStore>
-      </WalletStore>
+        <WalletStore>
+          <InputStore>
+            <MessageStore>
+              <MenuStore>
+                <Routes>
+                  <Route path="/" element={<VM />} />
+                  <Route path="/wallet" element={<Wallet />} />
+                  {/* TODO: 없는 페이지 들어갔을 때 NotFound 페이지 출력 후 몇 초 후 홈으로 리다이렉트 되는 식으로 처리 */}
+                  {/* <Route path="/*" element={<NotFound />} /> */}
+                  <Route path="*" element={<Navigate to="/" replace />} />
+                </Routes>
+              </MenuStore>
+            </MessageStore>
+          </InputStore>
+        </WalletStore>
+      </TimerStore>
     </BrowserRouter>
   );
 }
