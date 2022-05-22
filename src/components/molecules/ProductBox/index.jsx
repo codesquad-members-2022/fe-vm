a43d05e28b215f6837673dc5074c1fa30eb062d2
Hemudi <ksum1205@naver.com>
2022-05-22 14:46:32 +0900

    Refactor: 사용하지 않는 import 제거

--- src/components/molecules/ProductBox/index.jsx
+++ src/components/molecules/ProductBox/index.jsx
@@ -3,13 +3,10 @@ import PropTypes from 'prop-types';
 import Label from 'components/atoms/Label';
 import Button from 'components/atoms/Button';
 import * as Styled from 'components/molecules/ProductBox/ProductBox.style';
-import { WalletContext } from 'context/Wallet';
 
 const SOLD_OUT_TEXT = 'SOLD OUT';
 
 const ProductBox = ({ icon, cost, isSoldOut, isActive, ...props }) => {
-  const { state: walletState } = useContext(WalletContext);
-
   const buttonStyle = {
     sizeType: 'thin',
     borderType: 'rounded',
