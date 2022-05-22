52c1b64c6a15db15c263188e5091a1803b531148
Hemudi <ksum1205@naver.com>
2022-05-22 13:18:59 +0900

    Fix: React StrictMode 주석 수정

--- src/index.js
+++ src/index.js
@@ -8,9 +8,9 @@ import GlobalStyle from 'assets/style/global';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   // <React.StrictMode>
-    <ThemeProvider theme={defaultTheme}>
-      <GlobalStyle />
-      <App />
-    </ThemeProvider>
-  {/* </React.StrictMode> */}
+  <ThemeProvider theme={defaultTheme}>
+    <GlobalStyle />
+    <App />
+  </ThemeProvider>
+  // </React.StrictMode>
 );
